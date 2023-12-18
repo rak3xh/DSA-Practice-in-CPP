08712f6606971918bafe97bc99b7a40cc4ffb331
rak3xh <115797199+rak3xh@users.noreply.github.com>
2023-12-18 22:38:31 +0530

    18 dec

--- c/add.c
+++ c/add.c
@@ -2,9 +2,10 @@
 #include <conio.h>
 int main()
 {
-    int a, b, c = 0;
+    int a, b, c = 0, d = 1;
     scanf("%d%d", &a, &b);
     c = a + b;
+    d = a - b;
     printf("\n %d", c);
     return 0;
 }
