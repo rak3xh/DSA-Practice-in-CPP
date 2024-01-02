8c41f7c8e10785b39ecce0878949788b157981fe
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-01-02 18:46:16 +0530

    added 18:46 2 jan

---
+++ c++/printnumberifeven.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i;
+    cout << "Enter the number between which it has to be printed (0-n)";
+    cin >> n;
+    for (i = 2; i <= n; (i++))
+    {
+        cout << i << " ";
+        i = i + 1;
+    }
+    return 0;
+}
