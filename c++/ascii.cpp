a702a92861d95d6dd4332bc2a2000fab4c8b4078
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-01-03 23:33:15 +0530

    updated 3 jan 23:33

---
+++ c++/ascii.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout << "Enter the character that ASCII value need to be checked:";
+    cin >> c;
+    cout << "The ASCII value of " << c << " is :" << int(c);
+}
