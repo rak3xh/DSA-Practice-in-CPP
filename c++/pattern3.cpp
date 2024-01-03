7e6b2f74f8a164ebfd0760385087c8644e8586d0
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-01-04 01:14:06 +0530

    1:13 04 jan

---
+++ c++/pattern3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i = 1;
+    cin >> n;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << j << " ";
+            j++;
+        }
+        cout << "\n";
+        i++;
+    }
+}
