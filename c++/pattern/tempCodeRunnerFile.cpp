05fd505c9b6f18cb37990d64d60727db17641210
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-01-05 01:17:42 +0530

    1:17 05 jan

---
+++ c++/pattern/tempCodeRunnerFile.cpp
@@ -0,0 +1,23 @@
+int main()
+{
+    int n, i = 1;
+    cin >> n;
+    while (i <= n)
+    {
+        int space = n - i;
+        while (space)
+        {
+            cout << " ";
+            space--;
+        }
+        int j = i;
+        while (j)
+        {
+
+            cout << "*";
+            j--;
+        }
+        cout << "\n";
+        i++;
+    }
+}
