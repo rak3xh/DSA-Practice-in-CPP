42f2ce7d73676253f5c6dda2b7f6ec68ece7124f
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-01-06 00:18:10 +0530

    00:17 6 jan

---
+++ c++/basic/forloopoutput.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /* for (int i = 0; i <= 5; i++)
+     {
+         cout << i << " ";
+         i++;
+     }*/
+    /*for (int i = 0; i <= 5; i--)
+    {
+        cout << i << " ";
+        i++;
+    }*/
+    for (int i = 0; i <= 15; i += 2)
+    {
+        cout << i << " ";
+        if (i & 1)
+        {
+            continue;
+        }
+        i++;
+    }
+}
