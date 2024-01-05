fc819920a075774bdb4bde2fa3a362e987f789ba
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-01-06 02:50:18 +0530

    2:50 6 dec

---
+++ c++/binary to decimal/decToBinary2nd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int ans = 0, i = 0;
+    while (n != 0)
+    {
+        int digit = n & 1;
+        n = n >> 1;
+        ans = ans + (pow(10, i) * digit);
+        i++;
+    }
+    cout << "The binary is:" << ans;
+}
