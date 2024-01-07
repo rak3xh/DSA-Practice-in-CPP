11f1d0bcb4cf656b1d59922b5dd964bc096f948d
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-01-08 03:27:38 +0530

    3:27 08 jan

---
+++ c++/basic/calculatorusingswtch.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char flag = 'y';
+    do
+    {
+        int a, b, opans1_2 = 0, opans4 = 1;
+        float opans3 = 1.0;
+        char op;
+        cout << "Enter the Value of the first number: " << endl;
+        std::cin >> a;
+        cout << "Enter the Value of the Second number: " << endl;
+        std::cin >> b;
+        cout << "Enter the Operation you want to do [+, -, *, /, %] : " << endl;
+        std::cin >> op;
+        switch (op)
+        {
+        case '+':
+        {
+            opans1_2 = a + b;
+            cout << endl
+                 << "The Sum of the two input numbers is :" << opans1_2;
+
+            break;
+        }
+        case '-':
+        {
+            opans1_2 = a - b;
+            cout << endl
+                 << "The difference of the two input numbers is :" << opans1_2;
+            break;
+        }
+        case '*':
+        {
+            opans3 = a * b;
+            cout << endl
+                 << "The product of the two input numbers is :" << opans3;
+            break;
+        }
+        case '/':
+        {
+            if (b == 0)
+            {
+                cout << "The number cannot be divided by zero!!!";
+                break;
+            }
+            else
+            {
+                opans4 = float(a) / float(b);
+                cout << endl
+                     << "The result of division of the two input numbers is :" << opans4;
+                break;
+            }
+        }
+        case '%':
+        {
+            opans3 = a % b;
+            cout << endl
+                 << "The remainder of the two input numbers is :" << opans3;
+            break;
+        }
+        default:
+            cout << "Enter a valid Operator!!!";
+            break;
+        }
+        cout << endl
+             << "Do you want to continue :";
+        std::cin >> flag;
+    } while ((flag == 'y') || (flag == 'Y'));
+    return 0;
+}
