25623a3bbd0d4a794473553b659cdb08298ef4b1
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-01-12 00:26:53 +0530

    00:26 12 jan

---
+++ c++/arrays/sizeof.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int ar[] = {10, 20, 30, 40, 50, 60};
+
+    cout << endl
+         << "Array Size accorsing to index: " << sizeof(ar) / sizeof(int);
+    cout << endl
+         << "Total array size: " << sizeof(ar);
+}
