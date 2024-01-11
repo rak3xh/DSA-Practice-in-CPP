2e1b8ebd57439fd794249288d023240124b3e70b
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-01-12 03:00:27 +0530

    03:00 12 jan

---
+++ c++/arrays/rotatearray.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+void reverse(int a[], int n, int k)
+{
+
+    for (int i = 0; i < k; i++)
+    {
+        int start = (n - 1);
+        int next = start - 1;
+        while (start > 0)
+        {
+            swap(a[start], a[next]);
+            start--;
+            next--;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n, a[50];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int k;
+    cin >> k;
+    reverse(a, n, k);
+}
