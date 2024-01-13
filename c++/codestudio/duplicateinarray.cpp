180e44808a6ea76791eff1a34a3d00b93e7a552a
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-01-13 06:52:54 +0530

    06:42 13 jan

---
+++ c++/codestudio/duplicateinarray.cpp
@@ -0,0 +1,19 @@
+
+// solutuion of the problem using XOR operator
+
+/*
+int findDuplicate(vector<int> &arr)
+{
+    // Write your code here
+    int ans=0;
+    for(int i=0;i<arr.size();i++){
+        ans^=arr[i];
+    }
+    for(int i=1;i<arr.size();i++){
+        ans^=i;
+    }
+    return ans;
+
+}
+
+*/
