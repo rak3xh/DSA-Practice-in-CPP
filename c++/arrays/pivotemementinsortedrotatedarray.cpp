e340b7bd868b86d1ec032b85dd5a0149d0bf9509
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-01-20 06:03:36 +0530

    06:03 20 jan

---
+++ c++/arrays/pivotemementinsortedrotatedarray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int pivotfind(int a[], int n)
+{
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    while (s < e)
+    {
+        if (a[mid] < a[mid + 1])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
+int main()
+{
+    int a[50], n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int pivot = pivotfind(a, n);
+    cout << "The pivot Element is :" << a[pivot];
+}
