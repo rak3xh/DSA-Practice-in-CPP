d9e90083b6039947aecafce3a856c8d8db2f4992
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-01-26 02:18:20 +0530

    02:18 26 jan

---
+++ c++/codestudio/termsofAP.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+vector<int> termsOfAP(int x)
+{
+    // Write your code here.
+    int j = 1;
+
+    vector<int> ans;
+
+    int count = 0;
+
+    while (count < x)
+    {
+
+        int p = (3 * j) + 2;
+
+        if (p % 4 != 0)
+        {
+
+            ans.push_back(p);
+
+            j++;
+
+            count++;
+        }
+        else
+        {
+
+            j++;
+        }
+    }
+
+    return ans;
+}
