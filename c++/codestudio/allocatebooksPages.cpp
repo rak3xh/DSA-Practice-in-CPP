96007d1d684ce11066bf011f24d6b99990c21730
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-02-01 04:18:42 +0530

    04:18 01 Feb

---
+++ c++/codestudio/allocatebooksPages.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool PossibleSolution(vector<int> &pages, int n, int m, int mid)
+{
+    int StudentCount = 1;
+    int totalPages = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (totalPages + pages[i] < mid)
+        {
+            totalPages += pages[i];
+        }
+        else
+        {
+            StudentCount++;
+            if (StudentCount > m || pages[i] > mid)
+            {
+                return false;
+            }
+            totalPages = pages[i];
+        }
+    }
+    return true;
+}
+int AllocatePages(vector<int> &pages, int n, int m)
+{
+    int s = 0;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += pages[i];
+    }
+    int e = sum;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (PossibleSolution(pages, n, m, mid))
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> pages{12, 34, 67, 90};
+    int n = 4;
+    int m = 2;
+    int ans = AllocatePages(pages, n, m);
+    cout << ans;
+}
