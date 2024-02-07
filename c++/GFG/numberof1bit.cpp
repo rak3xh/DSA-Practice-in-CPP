625ba689095a84dd1b3a7d1e4fbdf3a2555fc093
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-02-08 03:33:39 +0530

    03:33 08 Feb

---
+++ c++/GFG/numberof1bit.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int setBits(int N)
+    {
+        // Write Your Code here
+        int count = 0;
+        while (N != 0)
+        {
+            if (N & 1)
+            {
+                count++;
+            }
+            N = N >> 1;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
