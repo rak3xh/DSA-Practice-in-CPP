584efe12d5ceb8ade39924e8e6f2ae09013370a8
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-02-11 23:52:42 +0530

    23:52 11 Feb

---
+++ c++/sorting/selectionsort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void selectionSort(int a[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[minIndex])
+            {
+                swap(a[minIndex], a[j]);
+            }
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int a[20];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    selectionSort(a, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
