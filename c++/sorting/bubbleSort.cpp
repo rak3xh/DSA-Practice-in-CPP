606706f1c47c679bbf074a87314cb3c2c2da40ec
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-02-12 02:07:08 +0530

    02:07 12 Feb

--- c++/sorting/bubbleSort.cpp
+++ c++/sorting/bubbleSort.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 void BubbleSort(int a[], int n)
 {
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n - 1; i++) // for(int i=1;i<n;i++)
     {
-        for (int j = 0; j < n - i; j++)
+        for (int j = 0; j < n - i - 1; j++) // for(int j=0;j<n-i;j++)
         {
             if (a[j] > a[j + 1])
             {
