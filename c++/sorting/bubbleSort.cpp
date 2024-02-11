1e78500f2f5754094786a1dad22659c989b42192
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-02-12 01:50:31 +0530

    01:50 12 Feb

---
+++ c++/sorting/bubbleSort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void BubbleSort(int a[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }
+}
+int main()
+{
+    int n;
+    int a[20];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    BubbleSort(a, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
