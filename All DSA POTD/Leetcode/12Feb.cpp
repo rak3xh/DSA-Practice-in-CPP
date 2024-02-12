9bf5ebdbf3e90e92101c4e89397bdbf9f28fbb59
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-02-12 16:44:43 +0530

    16:44 12 Feb

---
+++ All DSA POTD/Leetcode/12Feb.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int ans;
+        int count = 0;
+
+        for (const int num : nums)
+        {
+            if (count == 0)
+                ans = num;
+            count += num == ans ? 1 : -1;
+        }
+
+        return ans;
+    }
+};
