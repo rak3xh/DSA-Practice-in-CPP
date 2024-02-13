34815d12836fd3a0a02e6abda3cd4dbe70b84bce
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-02-13 05:55:01 +0530

    05:54 13 Feb

---
+++ All DSA POTD/Leetcode/13Feb.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string firstPalindrome(vector<string> &words)
+    {
+        for (const string &word : words)
+            if (isPalindrome(word))
+                return word;
+        return "";
+    }
+
+private:
+    bool isPalindrome(const string &s)
+    {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j)
+            if (s[i++] != s[j--])
+                return false;
+        return true;
+    }
+};
