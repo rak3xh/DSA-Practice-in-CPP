b2d56166e7045206e349a90c7e878e6f01b8421f
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-02-20 11:12:10 +0530

    11:12 20 Feb

---
+++ All DSA POTD/Leetcode/20Feb.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int ans = nums.size();
+
+        for (int i = 0; i < nums.size(); ++i)
+            ans ^= i ^ nums[i];
+
+        return ans;
+    }
+};
