c9a9aacab1cca334191fd3ad9870f769c58d3a8a
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-02-21 05:54:54 +0530

    05:54 21 Feb

---
+++ All DSA POTD/Leetcode/21Feb.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int rangeBitwiseAnd(int left, int right)
+    {
+        int shiftBits = 0;
+
+        while (left != right)
+        {
+            left >>= 1;
+            right >>= 1;
+            ++shiftBits;
+        }
+
+        return left << shiftBits;
+    }
+};
