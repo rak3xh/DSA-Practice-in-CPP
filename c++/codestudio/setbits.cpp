e7b63c2d2641dfb34c557c4657bb4bbdf5a5ec13
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-02-25 02:24:41 +0530

    02:24 25 Feb

---
+++ c++/codestudio/setbits.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+
+using namespace std;
+
+// Write your countBits function here
+
+int countBits(int n)
+
+{
+
+    int count = 0;
+
+    while (n != 0)
+
+    {
+
+        if (n & 1)
+
+        {
+
+            count++;
+        }
+
+        n = n >> 1;
+    }
+
+    return count;
+}
+
+int main()
+{
+
+    int n;
+
+    cin >> n;
+
+    cout << countBits(n);
+
+    return 0;
+}
