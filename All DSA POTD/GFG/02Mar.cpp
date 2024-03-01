6b5a980bd03c6ce35057537e7727e8545ae5a1d2
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-03-02 03:12:10 +0530

    03:12 02 Mar

---
+++ All DSA POTD/GFG/02Mar.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int firstElementKTime(int n, int k, int a[])
+    {
+        map<int, int> m;
+        for (int i = 0; i < n; i++)
+        {
+            m[a[i]]++;
+            if (m[a[i]] >= k)
+            {
+                return a[i];
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        Solution ob;
+        cout << ob.firstElementKTime(n, k, a) << endl;
+    }
+
+    return 0;
+}
