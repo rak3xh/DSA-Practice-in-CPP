1a1335775ad7137af23e8fb18b9875906b394377
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-03-03 01:48:04 +0530

    01:47 03 Mar.cpp

---
+++ All DSA POTD/GFG/03Mar.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution
+{
+public:
+    // The main function that returns the arrangement with the largest value as
+    // string.
+    // The function accepts a vector of strings
+    static bool cmp(string a, string b)
+    {
+        return a + b > b + a;
+    }
+    string printLargest(int n, vector<string> &arr)
+    {
+        // code here
+        sort(arr.begin(), arr.end(), cmp);
+        string ans = "";
+        for (int i = 0; i < n; i++)
+        {
+            ans += arr[i];
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, i;
+        cin >> n;
+        vector<string> arr(n);
+        for (i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.printLargest(n, arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}
