6aac6255549fc53da457476c0ffa82eddc7f3fd9
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-03-02 15:53:28 +0530

    15:53 02 Mar

---
+++ All DSA POTD/Leetcode/02Mar.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> sortedSquares(vector<int> &nums)
+    {
+        const int n = nums.size();
+        vector<int> ans(n);
+        int i = n - 1;
+
+        for (int l = 0, r = n - 1; l <= r;)
+            if (abs(nums[l]) > abs(nums[r]))
+                ans[i--] = nums[l] * nums[l++];
+            else
+                ans[i--] = nums[r] * nums[r--];
+
+        return ans;
+    }
+};
