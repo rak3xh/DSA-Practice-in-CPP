1d58b76f6b83670b3f5021a7e920fa3ee11650d9
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-03-06 02:58:41 +0530

    02:58 06 Mar

---
+++ All DSA POTD/Leetcode/06Mar.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minimumLength(string s)
+    {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j && s[i] == s[j])
+        {
+            const char c = s[i];
+            while (i <= j && s[i] == c)
+                ++i;
+            while (i <= j && s[j] == c)
+                --j;
+        }
+
+        return j - i + 1;
+    }
+};
