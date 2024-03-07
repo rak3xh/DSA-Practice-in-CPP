5c79b525a8dbfd45576c7567c36e1a04ac4b6d54
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-03-08 02:53:48 +0530

    02:53 08 Mar

---
+++ c++/codestudio/DuplicateInArray2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int findDuplicate(vector<int> &arr)
+{
+    // Write your code here
+    int ans = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        ans ^= arr[i];
+    }
+    for (int i = 1; i < arr.size(); i++)
+    {
+        ans ^= i;
+    }
+    return ans;
+}
