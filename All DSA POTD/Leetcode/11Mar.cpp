03fa7d9d60d8f891bf0ea16b20969886e8ca8cc8
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-03-11 07:01:46 +0530

    07:01 11 Mar

---
+++ All DSA POTD/Leetcode/11Mar.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string customSortString(string order, string s)
+    {
+        string ans;
+        vector<int> count(128);
+
+        for (const char c : s)
+            ++count[c];
+
+        for (const char c : order)
+            while (count[c]-- > 0)
+                ans += c;
+
+        for (char c = 'a'; c <= 'z'; ++c)
+            while (count[c]-- > 0)
+                ans += c;
+
+        return ans;
+    }
+};
