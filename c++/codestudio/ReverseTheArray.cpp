53476dbe3c1b4133952d4ab2dfbe9644749bcf2e
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-03-12 02:36:40 +0530

    02:36 12 Mar

---
+++ c++/codestudio/ReverseTheArray.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseArray(vector<int> &arr, int m)
+{
+
+    // Write your code here.
+
+    int s = m + 1;
+
+    int e = arr.size() - 1;
+
+    while (s < e)
+    {
+
+        swap(arr[s], arr[e]);
+
+        s++;
+
+        e--;
+    }
+}
