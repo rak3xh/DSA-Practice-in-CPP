b7d1ec1ca580963c4fbc8418266fc6af0ab3a1e9
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-03-13 06:17:49 +0530

    06:17 13 Mar

---
+++ All DSA POTD/Leetcode/13Mar.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int pivotInteger(int n)
+    {
+        const int y = (n * n + n) / 2;
+        const int x = sqrt(y);
+        return x * x == y ? x : -1;
+    }
+};
