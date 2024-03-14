80d202db89fe0bf0de628a289e0d4cacd2565c80
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-03-14 23:05:23 +0530

    23:05 15 Mar

---
+++ All DSA POTD/Leetcode/14Mar.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+/*
+
+class Solution
+{
+public:
+    int numSubarraysWithSum(vector<int> &nums, int goal)
+    {
+        int ans = 0;
+        int prefix = 0;
+        // {prefix: number of occurrence}
+        unordered_map<int, int> count{{0, 1}};
+
+        for (const int num : nums)
+        {
+            prefix += num;
+            if (const auto it = count.find(prefix - goal); it != count.cend())
+                ans += it->second;
+            ++count[prefix];
+        }
+
+        return ans;
+    }
+};
+*/
