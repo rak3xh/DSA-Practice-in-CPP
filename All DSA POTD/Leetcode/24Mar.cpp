39fb087e81b0f0b02b2d9298198be0f10834c2aa
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-03-24 12:04:37 +0530

    12:04 24 Mar

---
+++ All DSA POTD/Leetcode/24Mar.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        int slow = nums[nums[0]];
+        int fast = nums[nums[nums[0]]];
+
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        slow = nums[0];
+
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
