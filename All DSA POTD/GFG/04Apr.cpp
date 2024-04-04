6dc76c9f6d5d765dddb920779cc5a6d3ea4b005c
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-04-04 22:45:10 +0530

    22:45 04 Apr

---
+++ All DSA POTD/GFG/04Apr.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    // Function to find sum of all possible substrings of the given string.
+    long long sumSubstrings(string s)
+    {
+
+        // your code here
+        int mod = 1e9 + 7;
+        long long ans = 0, prev = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            int num = s[i] - '0';
+            prev = ((prev * 10) % mod + (num * (i + 1)) % mod) % mod;
+            ans = (ans % mod + prev % mod) % mod;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    // taking total testcases
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        // taking string
+        string s;
+        cin >> s;
+        Solution ob;
+        // calling sumSubstrings() function
+        cout << ob.sumSubstrings(s) << endl;
+    }
+    return 0;
+}
