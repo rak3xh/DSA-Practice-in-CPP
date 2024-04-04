e84ea8dea6da4e26944252b3129d2f7c4672c2f0
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-04-04 22:41:29 +0530

    22:41 04 Apr

---
+++ All DSA POTD/Leetcode/04Apr.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxDepth(string s)
+    {
+        int ans = 0;
+        int opened = 0;
+
+        for (const char c : s)
+            if (c == '(')
+                ans = max(ans, ++opened);
+            else if (c == ')')
+                --opened;
+
+        return ans;
+    }
+};
