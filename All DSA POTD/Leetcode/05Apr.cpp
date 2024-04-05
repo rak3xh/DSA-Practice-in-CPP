ac0de9da77c57f0ca8ac7fe9c8970aadce253cf6
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-04-05 19:05:58 +0530

    19:05 05 Apr

---
+++ All DSA POTD/Leetcode/05Apr.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <string.h>
+using namespace std;
+/*
+class Solution
+
+{
+public:
+    string makeGood(string s)
+    {
+        string ans;
+        for (const char c : s)
+            if (!ans.empty() && isBadPair(ans.back(), c))
+                ans.pop_back();
+            else
+                ans.push_back(c);
+        return ans;
+    }
+
+    bool isBadPair(char a, char b)
+    {
+        return a != b && tolower(a) == tolower(b);
+    }
+};
+*/
