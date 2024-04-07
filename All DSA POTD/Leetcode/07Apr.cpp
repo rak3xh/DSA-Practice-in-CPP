9ef801cff94883802cbbe5e26ab7c042b9e53f8f
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-04-07 15:44:35 +0530

    15:44 07 Apr

---
+++ All DSA POTD/Leetcode/07Apr.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool checkValidString(string s)
+    {
+        int low = 0;  // the lower bound of the number of valid '('s
+        int high = 0; // the upper bound of the number of valid '('s
+
+        for (const char c : s)
+        {
+            switch (c)
+            {
+            case '(':
+                ++low;
+                ++high;
+                break;
+            case ')':
+                low = max(0, --low);
+                --high;
+                break;
+            case '*':
+                low = max(0, --low);
+                ++high;
+                break;
+            }
+            if (high < 0)
+                return false;
+        }
+
+        return low == 0;
+    }
+};
