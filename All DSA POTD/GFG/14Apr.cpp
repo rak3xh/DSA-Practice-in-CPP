c89bb86ad02770eae2ad1b9b591d83308293b66c
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-04-14 01:05:35 +0530

    01:05 14 Apr

---
+++ All DSA POTD/GFG/14Apr.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    void printArr(int n, int arr[])
+    {
+        // Your code for printing array here
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    void setToZero(int n, int arr[])
+    {
+        // Use memset to set arr to zero
+        memset(arr, 0, 4 * n);
+    }
+
+    void xor1ToN(int n, int arr[])
+    {
+        // Xor arr[i]^i
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = arr[i] ^ i;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution obj;
+        obj.xor1ToN(n, arr);
+        obj.printArr(n, arr);
+        obj.setToZero(n, arr);
+        obj.printArr(n, arr);
+    }
+    return 0;
+}
