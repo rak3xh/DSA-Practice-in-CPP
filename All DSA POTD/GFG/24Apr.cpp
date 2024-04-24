864f42a7d3d2c4009c029cbad68d77f9af5d4b8c
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-04-24 21:53:50 +0530

    21:53 24 Apr

---
+++ All DSA POTD/GFG/24Apr.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    int ways(int x, int y)
+    {
+        // code here.
+        vector<vector<int>> dp(x + 1, vector<int>(y + 1, -1));
+        return solve(x, y, dp);
+    }
+    int solve(int x, int y, vector<vector<int>> &dp)
+    {
+        if (x == 0 && y == 0)
+        {
+            return 1;
+        }
+        if (x < 0 || y < 0)
+        {
+            return 0;
+        }
+        if (dp[x][y] != -1)
+        {
+            return dp[x][y];
+        }
+        dp[x][y] = (solve(x - 1, y, dp) + solve(x, y - 1, dp)) % 1000000007;
+        return dp[x][y];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        Solution ob;
+        cout << ob.ways(x, y) << endl;
+    }
+    return 0;
+}
