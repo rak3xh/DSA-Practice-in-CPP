897b4b2f3bd6b0074fceb9e2d52214fcf06b60b8
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-04-29 19:26:12 +0530

    19:26 29 Apr

---
+++ All DSA POTD/Leetcode/29Apr.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums, int k)
+    {
+        const int xors = accumulate(nums.begin(), nums.end(), 0, bit_xor<>());
+        return __builtin_popcount(k ^ xors);
+    }
+};
