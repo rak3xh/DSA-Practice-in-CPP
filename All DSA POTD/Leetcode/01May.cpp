4cf093d7fbe9208fcae8f9041fda7395390f0248
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-05-01 14:08:00 +0530

    14:07 01 May

---
+++ All DSA POTD/Leetcode/01May.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string reversePrefix(string word, char ch)
+    {
+        reverse(word.begin(), word.begin() + word.find(ch) + 1);
+        return word;
+    }
+};
