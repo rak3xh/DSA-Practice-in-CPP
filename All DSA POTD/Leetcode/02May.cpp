700fb3682cc692c698f95a9085e5f7b5e1224129
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-05-02 23:03:45 +0530

    23:03 02 May

---
+++ All DSA POTD/Leetcode/02May.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int findMaxK(vector<int> &nums)
+    {
+        int ans = -1;
+        unordered_set<int> seen;
+
+        for (const int num : nums)
+            if (seen.count(-num))
+                ans = max(ans, abs(num));
+            else
+                seen.insert(num);
+
+        return ans;
+    }
+};
