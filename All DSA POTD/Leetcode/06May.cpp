1d1525e02dcefa9a95e345c3c89cec9f9bc0cdf3
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-05-06 18:47:33 +0530

    18:47 06 May

---
+++ All DSA POTD/Leetcode/06May.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *removeNodes(ListNode *head)
+    {
+        if (head == nullptr)
+            return nullptr;
+        head->next = removeNodes(head->next);
+        return head->next != nullptr && head->val < head->next->val ? head->next
+                                                                    : head;
+    }
+};
