5baa38f27494a3a10ff4c625215f35bb986487ad
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-05-20 18:46:08 +0530

    18:46 20 May

---
+++ All DSA POTD/Leetcode/20May.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    long long int PowMod(long long int x, long long int n, long long int M)
+    {
+        // Code here
+        long long int ans = 1;
+
+        x = x % M;
+
+        while (n)
+        {
+            if (n & 1)
+            {
+                ans = (ans * x) % M;
+            }
+            x = (x * x) % M;
+            n = n >> 1;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        long long int x, n, m;
+        cin >> x >> n >> m;
+        Solution ob;
+        long long int ans = ob.PowMod(x, n, m);
+        cout << ans << "\n";
+    }
+    return 0;
+}
