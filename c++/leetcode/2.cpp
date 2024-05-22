d4ef34c3ca496ebff98daeb310dad02941515a27
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-05-22 19:27:24 +0530

    19:27 22 May

---
+++ c++/leetcode/2.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode dummy(0);
+        ListNode *curr = &dummy;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry > 0)
+        {
+            if (l1 != nullptr)
+            {
+                carry += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr)
+            {
+                carry += l2->val;
+                l2 = l2->next;
+            }
+            curr->next = new ListNode(carry % 10);
+            carry /= 10;
+            curr = curr->next;
+        }
+
+        return dummy.next;
+    }
+};
