e0a998a49fa3cd49741459a6c184cefe6382406a
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-05-27 19:15:32 +0530

    19:15 27 May

---
+++ All DSA POTD/Leetcode/27May.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int specialArray(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+
+        if (nums[0] >= nums.size())
+            return nums.size();
+
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            const int count = nums.size() - i;
+            if (nums[i - 1] < count && nums[i] >= count)
+                return count;
+        }
+
+        return -1;
+    }
+};
