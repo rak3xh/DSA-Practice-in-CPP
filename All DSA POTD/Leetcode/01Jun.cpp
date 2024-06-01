5de72296327dffa0c9dea1d5c085e3f70930cd62
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-06-01 12:12:43 +0530

    12:12 01 May

---
+++ All DSA POTD/Leetcode/01Jun.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int scoreOfString(string s)
+    {
+        int ans = 0;
+
+        for (int i = 1; i < s.length(); ++i)
+            ans += abs(s[i] - s[i - 1]);
+
+        return ans;
+    }
+};
