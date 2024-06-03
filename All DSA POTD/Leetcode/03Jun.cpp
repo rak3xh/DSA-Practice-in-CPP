dd4285fa5726e56cd6770344f82bc5ee8df7c777
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-06-03 19:29:46 +0530

    19:29 03 May

---
+++ All DSA POTD/Leetcode/03Jun.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int appendCharacters(string s, string t)
+    {
+        int i = 0; // t's index
+
+        for (const char c : s)
+            if (c == t[i])
+                if (++i == t.length())
+                    return 0;
+
+        return t.length() - i;
+    }
+};
