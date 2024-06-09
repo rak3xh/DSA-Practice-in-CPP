0090f80efa76e51c8f95986e7e3863b3d7ead727
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-06-09 22:01:10 +0530

    22:01 09 Jun

---
+++ All DSA POTD/Leetcode/09Jun.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int subarraysDivByK(vector<int> &nums, int k)
+    {
+        int ans = 0;
+        int prefix = 0;
+        vector<int> count(k);
+        count[0] = 1;
+
+        for (const int num : nums)
+        {
+            prefix = (prefix + num % k + k) % k;
+            ans += count[prefix];
+            ++count[prefix];
+        }
+
+        return ans;
+    }
+};
