ed0e7d5845a774dbc190c76e355a6b7842f06f37
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-06-14 18:55:37 +0530

    18:55 14 Jun

---
+++ All DSA POTD/Leetcode/14Jun.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minIncrementForUnique(vector<int> &nums)
+    {
+        int ans = 0;
+        int minAvailable = 0;
+
+        sort(nums.begin(), nums.end());
+
+        for (const int num : nums)
+        {
+            ans += max(minAvailable - num, 0);
+            minAvailable = max(minAvailable, num) + 1;
+        }
+
+        return ans;
+    }
+};
