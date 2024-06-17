21754d0da07278dfb2fd8d876acde472b84e93e9
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-06-17 20:48:35 +0530

    20:48 17 Jun

---
+++ All DSA POTD/Leetcode/17Jun.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool judgeSquareSum(int c)
+    {
+        unsigned l = 0;
+        unsigned r = sqrt(c);
+
+        while (l <= r)
+        {
+            const unsigned sum = l * l + r * r;
+            if (sum == c)
+                return true;
+            if (sum < c)
+                ++l;
+            else
+                --r;
+        }
+
+        return false;
+    }
+};
