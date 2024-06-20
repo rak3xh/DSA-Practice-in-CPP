16e07d406357ab88d52abe865252e46a323f28e4
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-06-20 18:41:23 +0530

    18:41 20 Jun

---
+++ All DSA POTD/Leetcode/20Jun.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxDistance(vector<int> &position, int m)
+    {
+        sort(position.begin(), position.end());
+        int l = 1;
+        int r = position.back() - position.front();
+
+        while (l < r)
+        {
+            const int mid = r - (r - l) / 2;
+            if (numBalls(position, mid) >= m) // There're too many balls.
+                l = mid;
+            else // There're too few balls.
+                r = mid - 1;
+        }
+
+        return l;
+    }
+
+private:
+    int numBalls(const vector<int> &position, int force)
+    {
+        int balls = 0;
+        int prevPosition = -force;
+        for (const int pos : position)
+            if (pos - prevPosition >= force)
+            {
+                ++balls;
+                prevPosition = pos;
+            }
+        return balls;
+    }
+};
