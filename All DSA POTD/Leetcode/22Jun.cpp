6cfa9df448720e10cb3a8d05171971406a0cfb44
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-06-22 15:41:37 -0300

    15:41 22 Jun

---
+++ All DSA POTD/Leetcode/22Jun.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int numberOfSubarrays(vector<int> &nums, int k)
+    {
+        return numberOfSubarraysAtMost(nums, k) -
+               numberOfSubarraysAtMost(nums, k - 1);
+    }
+
+private:
+    int numberOfSubarraysAtMost(vector<int> &nums, int k)
+    {
+        int ans = 0;
+
+        for (int l = 0, r = 0; r <= nums.size();)
+            if (k >= 0)
+            {
+                ans += r - l;
+                if (r == nums.size())
+                    break;
+                if (nums[r] & 1)
+                    --k;
+                ++r;
+            }
+            else
+            {
+                if (nums[l] & 1)
+                    ++k;
+                ++l;
+            }
+
+        return ans;
+    }
+};
