3285a1ca7a4b0257ed7c30367e682d44b96d02f3
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-06-26 20:36:22 +0530

    20:36 26 Jun

---
+++ All DSA POTD/Leetcode/26Jun.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *balanceBST(TreeNode *root)
+    {
+        vector<int> nums;
+        inorder(root, nums);
+        return build(nums, 0, nums.size() - 1);
+    }
+
+private:
+    void inorder(TreeNode *root, vector<int> &nums)
+    {
+        if (root == nullptr)
+            return;
+        inorder(root->left, nums);
+        nums.push_back(root->val);
+        inorder(root->right, nums);
+    }
+
+    // Same as 108. Convert Sorted Array to Binary Search Tree
+    TreeNode *build(const vector<int> &nums, int l, int r)
+    {
+        if (l > r)
+            return nullptr;
+        const int m = (l + r) / 2;
+        return new TreeNode(nums[m], build(nums, l, m - 1), build(nums, m + 1, r));
+    }
+};
