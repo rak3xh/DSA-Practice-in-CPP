b513926472c2e50c7a508af7317960a376e36d6c
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-07-06 11:07:25 +0530

    11:07 06 Jul

---
+++ All DSA POTD/Leetcode/06Jul.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int passThePillow(int n, int time)
+    {
+        time %= (n - 1) * 2;
+        if (time < n)
+            return 1 + time;
+        return n - (time - (n - 1));
+    }
+};
