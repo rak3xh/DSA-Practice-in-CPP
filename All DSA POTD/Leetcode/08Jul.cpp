63b320769ee9c1ac0ad2410875d29a5f64717be9
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-07-08 20:12:34 +0530

    20:12 08 Jul

---
+++ All DSA POTD/Leetcode/08Jul.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int findTheWinner(int n, int k)
+    {
+        vector<bool> friends(n);
+
+        int friendCount = n;
+        int fp = 0; // friends' index
+
+        while (friendCount > 1)
+        {
+            for (int i = 0; i < k; ++i, ++fp)
+                while (friends[fp % n]) // The friend is not there.
+                    ++fp;               // Point to the next one.
+            friends[(fp - 1) % n] = true;
+            --friendCount;
+        }
+
+        const auto it =
+            find_if(friends.begin(), friends.end(), [](int f)
+                    { return !f; });
+        return distance(friends.begin(), it) + 1;
+    }
+};
