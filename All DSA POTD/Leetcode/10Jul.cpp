30546df8771447c7af0a4f4ef8d255deb36343ac
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-07-10 19:28:07 +0530

    19:28 10 Jul

--- All DSA POTD/Leetcode/10Jul.cpp
+++ All DSA POTD/Leetcode/10Jul.cpp
@@ -1,2 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Solution
+{
+public:
+    int minOperations(vector<string> &logs)
+    {
+        int ans = 0;
+
+        for (const string &log : logs)
+        {
+            if (log == "./")
+                continue;
+            if (log == "../")
+                ans = max(0, ans - 1);
+            else
+                ++ans;
+        }
+
+        return ans;
+    }
+};
