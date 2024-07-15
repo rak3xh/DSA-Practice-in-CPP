f709e872a51aad6cfb3ef70d8a80be1d9c7960f5
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-07-15 19:39:00 +0530

    19:38 15 Jul

---
+++ All DSA POTD/Leetcode/15Jul.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *createBinaryTree(vector<vector<int>> &descriptions)
+    {
+        unordered_map<TreeNode *, TreeNode *> childToParent;
+        unordered_map<int, TreeNode *> valToNode;
+
+        for (const vector<int> &d : descriptions)
+        {
+            const int p = d[0];
+            const int c = d[1];
+            const bool isLeft = d[2];
+            TreeNode *parent =
+                valToNode.count(p) ? valToNode[p] : (valToNode[p] = new TreeNode(p));
+            TreeNode *child =
+                valToNode.count(c) ? valToNode[c] : (valToNode[c] = new TreeNode(c));
+            childToParent[child] = parent;
+            if (isLeft)
+                parent->left = child;
+            else
+                parent->right = child;
+        }
+
+        // Pick a random node and traverse upwardly.
+        TreeNode *root = childToParent.begin()->second;
+        while (childToParent.count(root))
+            root = childToParent[root];
+        return root;
+    }
+};
