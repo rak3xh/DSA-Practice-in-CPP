723a5c35a10d8bcc98b9a547cfb868a57559df66
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-07-22 18:34:43 +0530

    18:34 22 Jul

---
+++ All DSA POTD/Leetcode/22Jul.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<string> sortPeople(vector<string> &names, vector<int> &heights)
+    {
+        int n = names.size();
+        unordered_map<int, string> mapping;
+
+        for (int i = 0; i < n; ++i)
+        {
+            mapping[heights[i]] = names[i];
+        }
+
+        sort(heights.rbegin(), heights.rend());
+
+        for (int i = 0; i < n; ++i)
+        {
+            names[i] = mapping[heights[i]];
+        }
+
+        return names;
+    }
+};
