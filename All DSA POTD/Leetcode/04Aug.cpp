ea1426eb36c70250d046028e739b276fbe25438d
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-08-04 11:07:40 +0530

    11:07 04 Aug

---
+++ All DSA POTD/Leetcode/04Aug.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int rangeSum(vector<int> &nums, int n, int left, int right)
+    {
+        constexpr int kMod = 1'000'000'007;
+
+        auto subarraysAndSumNoGreaterThan = [&](int m) -> pair<int, long>
+        {
+            int count = 0;  // the number of subarrays <= m
+            long total = 0; // sum(subarrays)
+            int sum = 0;    // the current sum
+            int window = 0; // the window sum
+
+            for (int i = 0, j = 0; j < n; ++j)
+            {
+                sum += nums[j] * (j - i + 1);
+                window += nums[j]; // Extend each subarray that ends in j.
+                while (window > m)
+                {
+                    sum -= window;
+                    window -= nums[i++]; // Shrink the window.
+                }
+                count += j - i + 1;
+                total += sum;
+            }
+
+            return {count, total};
+        };
+
+        // [L, R] is the possible range of the sum of any subarray.
+        const int L = *min_element(nums.begin(), nums.end());
+        const int R = accumulate(nums.begin(), nums.end(), 0);
+
+        auto firstKSubarraysSum = [&](int k) -> long
+        {
+            int l = L;
+            int r = R;
+
+            while (l < r)
+            {
+                const int m = l + (r - l) / 2;
+                if (subarraysAndSumNoGreaterThan(m).first < k)
+                    l = m + 1;
+                else
+                    r = m;
+            }
+
+            pair<int, long> result = subarraysAndSumNoGreaterThan(l);
+            int count = result.first;
+            long total = result.second;
+            // If count != k, there're subarray(s) that have the same sum as l.
+            return total - l * (count - k);
+        };
+
+        return (firstKSubarraysSum(right) - firstKSubarraysSum(left - 1)) % kMod;
+    }
+};
