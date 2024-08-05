b6ced5bcfe88e3177fe7efa4b6ac90f47ccb517b
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-08-05 18:58:53 +0530

    18:58 05 Aug

---
+++ All DSA POTD/Leetcode/05Aug.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string kthDistinct(vector<string> &arr, int k)
+    {
+        unordered_map<string, int> count;
+
+        for (const string &a : arr)
+            ++count[a];
+
+        for (const string &a : arr)
+            if (count[a] == 1 && --k == 0)
+                return a;
+
+        return "";
+    }
+};
