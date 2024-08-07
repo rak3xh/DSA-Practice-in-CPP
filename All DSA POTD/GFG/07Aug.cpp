ceffe0627913b69921fb977eb479a0070a729bce
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-08-07 20:30:38 +0530

    20:30 07 Aug

---
+++ All DSA POTD/GFG/07Aug.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int kthElement(int k, vector<int> &arr1, vector<int> &arr2)
+    {
+        // code here
+        for (int it : arr2)
+            arr1.push_back(it);
+        sort(arr1.begin(), arr1.end());
+        return arr1[k - 1];
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        int n, m, k;
+        cin >> k;
+        cin.ignore();
+        string input;
+        int num;
+        vector<int> arr1, arr2;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num)
+        {
+            arr1.push_back(num);
+        }
+
+        getline(cin, input);
+        stringstream s3(input);
+        while (s3 >> num)
+        {
+            arr2.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.kthElement(k, arr1, arr2) << endl;
+    }
+    return 0;
+}
