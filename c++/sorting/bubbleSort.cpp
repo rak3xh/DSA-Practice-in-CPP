5d8670b42b3f1f1e37fd3172f6721cc18e3ca2ac
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-08-23 23:06:21 +0530

    23:06 23 Aug

--- c++/sorting/bubbleSort.cpp
+++ c++/sorting/bubbleSort.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 void BubbleSort(int a[], int n)
 {
-    for (int i = 0; i < n - 1; i++) // for(int i=1;i<n;i++)
+    // for (int i = 0; i < n - 1; i++)
+    // for (int j = 0; j < n - i - 1; j++)
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < n - i - 1; j++) // for(int j=0;j<n-i;j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
             if (a[j] > a[j + 1])
             {
--- c++/sorting/bubbleSortOptimized.cpp
+++ c++/sorting/bubbleSortOptimized.cpp
@@ -3,19 +3,21 @@
 using namespace std;
 void BubbleSort(int a[], int n)
 {
-    for (int i = 0; i < n - 1; i++) // for(int i=1;i<n;i++)
+    // for(int i=1;i<n;i++)
+    // for(int j=0;j<n-i;j++)
+    for (int i = 0; i < n - 1; i++)
     {
         bool swapped = false;
-        for (int j = 0; j < n - i - 1; j++) // for(int j=0;j<n-i;j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
             if (a[j] > a[j + 1])
             {
                 swap(a[j], a[j + 1]);
                 swapped = true;
             }
+            if (swapped == false)
+                break;
         }
-        if (swapped == false)
-            break;
     }
 }
 int main()
