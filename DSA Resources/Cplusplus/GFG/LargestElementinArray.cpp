190fc75fd48f13b2b114116c7382f86e543db22e
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-08-27 00:03:31 +0530

    00:03 27 Aug

---
+++ DSA Resources/Cplusplus/GFG/LargestElementinArray.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    int largest(vector<int> &arr)
+    {
+        // code here
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        return arr[n - 1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--)
+    {
+        string input;
+        int num;
+        vector<int> arr;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num)
+        {
+            arr.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.largest(arr) << endl;
+    }
+    return 0;
+}
