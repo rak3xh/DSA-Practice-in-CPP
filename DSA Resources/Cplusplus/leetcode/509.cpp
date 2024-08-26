6442c0d6c0063c73d0a03c18250f462fc67c15b4
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-08-26 23:53:48 +0530

    23:53 26 Aug

---
+++ DSA Resources/Cplusplus/leetcode/509.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        // Base cases
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        // Recursive case
+        return fib(n - 1) + fib(n - 2);
+    }
+};
