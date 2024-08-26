7788ed9ad78efa42838c8834f98bd7980aec5ba1
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-08-26 23:09:48 +0530

    23:09 26 Aug

---
+++ DSA Resources/Cplusplus/GFG/SumofAllDivisors.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    long long sumOfDivisors(int N)
+    {
+        // Write Your Code here
+        long long sum = 0;
+        for (int i = 1; i <= N; i++)
+        {
+            sum = sum + i * (N / i);
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        long long ans = ob.sumOfDivisors(N);
+        cout << ans << endl;
+    }
+    return 0;
+}
