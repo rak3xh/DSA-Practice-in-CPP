f9d6232477f35634add65fcd60f507f5e2bf271b
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-09-01 00:37:12 +0530

    00:37 01 Sep

--- DSA Resources/Cplusplus/sorting/bubbleSort.cpp
+++ DSA Resources/Cplusplus/sorting/bubbleSort.cpp
@@ -5,11 +5,11 @@ void BubbleSort(int a[], int n)
 {
     // for (int i = 0; i < n - 1; i++)
     // for (int j = 0; j < n - i - 1; j++)
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        for (int j = 0; j < n - i; j++)
         {
-            if (a[j] > a[j + 1])
+            if (a[j + 1] < a[j])
             {
                 swap(a[j], a[j + 1]);
             }
