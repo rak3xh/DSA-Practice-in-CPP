7fad25ecc723aad0d68ed4f41efd73a0d818215e
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-09-02 21:01:24 +0530

    21:01 02 Aug

---
+++ All DSA POTD/Leetcode/02Sep.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int chalkReplacer(vector<int> &chalk, int k)
+    {
+        k %= accumulate(chalk.begin(), chalk.end(), 0L);
+        if (k == 0)
+            return 0;
+
+        for (int i = 0; i < chalk.size(); ++i)
+        {
+            k -= chalk[i];
+            if (k < 0)
+                return i;
+        }
+
+        throw;
+    }
+};
