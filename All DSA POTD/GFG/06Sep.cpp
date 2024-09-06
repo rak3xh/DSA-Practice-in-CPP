1fbd383b20aca42aae43f45f992f09060a864ea3
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-09-06 21:12:29 +0530

    21:12 06 Sep

---
+++ All DSA POTD/GFG/06Sep.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution
+{
+public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr)
+    {
+        // code here...
+        int kadane = INT_MIN, count = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            count += arr[i];
+            kadane = max(kadane, count);
+            if (count < 0)
+                count = 0;
+        }
+        return kadane;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.maxSubarraySum(arr) << endl;
+    }
+}
