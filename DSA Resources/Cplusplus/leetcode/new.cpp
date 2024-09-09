d1848aca30ca52ac1a133b230c401663989d8b20
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-09-09 23:40:11 +0530

    23:40 09 Sep

---
+++ DSA Resources/Cplusplus/leetcode/new.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 0;
+        while (j < n - i)
+        {
+            cout << " ";
+            j++;
+        }
+        while (j < n)
+        {
+            cout << "#";
+            j++;
+        }
+        i++;
+        cout << endl;
+    }
+}
