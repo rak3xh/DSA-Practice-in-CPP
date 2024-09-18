7d878a99747f4956c8c51610775f280332ae28e9
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-09-18 21:11:58 +0530

    21:11 18 Sep

---
+++ All DSA POTD/GFG/18Sep.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    // Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        // Your code here
+        stack<char> st;
+        for (char &ch : x)
+        {
+            if (ch == '(' || ch == '[' || ch == '{')
+            {
+                st.push(ch);
+            }
+            else if (ch == ')')
+            {
+                if (st.empty() || st.top() != '(')
+                    return 0;
+                else
+                    st.pop();
+            }
+            else if (ch == '}')
+            {
+                if (st.empty() || st.top() != '{')
+                    return 0;
+                else
+                    st.pop();
+            }
+            else
+            {
+                if (st.empty() || st.top() != '[')
+                    return 0;
+                else
+                    st.pop();
+            }
+        }
+        return st.empty() ? 1 : 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    string a;
+    cin >> t;
+    while (t--)
+    {
+        cin >> a;
+        Solution obj;
+        if (obj.ispar(a))
+            cout << "balanced" << endl;
+        else
+            cout << "not balanced" << endl;
+    }
+}
