d1a75281a2669d673819090f68df038a8dbd3938
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-09-26 21:31:47 +0530

    21:31 26 Sep

---
+++ All DSA POTD/GFG/26Sep.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    // Function to find maximum number of consecutive steps
+    // to gain an increase in altitude with each step.
+    int maxStep(vector<int> &arr)
+    {
+        // Your code here
+        int count = 0;
+        int maxi = 0;
+
+        for (int i = 0; i < arr.size() - 1; i++)
+        {
+
+            if (arr[i] < arr[i + 1])
+            {
+                count++;
+            }
+            else
+            {
+                maxi = max(maxi, count);
+                count = 0;
+            }
+        }
+        maxi = max(maxi, count);
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+        Solution ob;
+        cout << ob.maxStep(arr) << endl;
+    }
+    return 0;
+}
