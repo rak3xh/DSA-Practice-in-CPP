34124431c0d38a5ed4be54fe7dc39e2a0cbe3643
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-09-29 14:23:46 +0530

    14:23 29 Sep

---
+++ All DSA POTD/GFG/29Sep.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution
+{
+public:
+    int totalCount(int k, vector<int> &arr)
+    {
+        // code here
+        int ans = 0;
+        for (auto it : arr)
+        {
+            ans += it / k;
+            if (it % k)
+                ans++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--)
+    {
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.totalCount(k, arr);
+        cout << res << endl;
+    }
+    return 0;
+}
