73bfc55587138937f68d7a781076268dd8bce9a1
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-10-08 21:04:41 +0530

    21:04 08 Oct

---
+++ All DSA POTD/Leetcode/08Oct.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minSwaps(string s)
+    {
+        // Cancel out all the matched pairs, then we'll be left with "]]]..[[[".
+        // The answer is ceil(the number of unmatched pairs / 2).
+        int unmatched = 0;
+
+        for (const char c : s)
+            if (c == '[')
+                ++unmatched;
+            else if (unmatched > 0) // c == ']' and there's a match.
+                --unmatched;
+
+        return (unmatched + 1) / 2;
+    }
+};
