0232f7b917de0dfabc7ec5a2595a8cf002826107
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-10-09 21:07:54 +0530

    21:07 09 Oct

---
+++ All DSA POTD/Leetcode/09Oct.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minAddToMakeValid(string s)
+    {
+        int l = 0;
+        int r = 0;
+
+        for (const char c : s)
+            if (c == '(')
+            {
+                ++l;
+            }
+            else
+            {
+                if (l == 0)
+                    ++r;
+                else
+                    --l;
+            }
+
+        return l + r;
+    }
+};
