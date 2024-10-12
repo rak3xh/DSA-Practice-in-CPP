19fe082d12c27474e29a4b995afc72d91bb817da
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-10-12 11:46:19 +0530

    11:46 12 Oct

--- All DSA POTD/Leetcode/12Oct.cpp
+++ All DSA POTD/Leetcode/12Oct.cpp
@@ -20,3 +20,26 @@ public:
         return minHeap.size();
     }
 };
+// c++ 20 Solution
+/*
+class Solution
+{
+public:
+    int minGroups(vector<vector<int>> &intervals)
+    {
+        priority_queue<int, vector<int>, greater<>> minHeap;
+
+        ranges::sort(intervals);
+
+        for (const vector<int> &interval : intervals)
+        {
+            // There's no overlap, so we can reuse the same group.
+            if (!minHeap.empty() && interval[0] > minHeap.top())
+                minHeap.pop();
+            minHeap.push(interval[1]);
+        }
+
+        return minHeap.size();
+    }
+};
+*/
