270b534593438a8c49ce03abb8b2d255073e1640
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-10-15 20:49:43 +0530

    20:49 15 Oct

---
+++ All DSA POTD/GFG/15Oct.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    // Function to count the number of subarrays which adds to the given sum.
+    int subArraySum(vector<int> &arr, int tar)
+    {
+        // Your code here
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int sum = 0;
+        int ans = 0;
+        for (auto &e : arr)
+        {
+            sum += e;
+            if (mp.count(sum - tar))
+            {
+                ans += mp[sum - tar];
+            }
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--)
+    {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int tar = stoi(ks);
+        Solution obj;
+        int res = obj.subArraySum(arr, tar);
+        cout << res << endl;
+    }
+    return 0;
+}
