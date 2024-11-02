f867ecf5008fdc1c443540f8dd96323db949744d
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-11-02 19:26:28 +0530

    19:26 02 Nov

---
+++ All DSA POTD/Leetcode/02Nov.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool isCircularSentence(string sentence)
+    {
+        for (int i = 0; i < sentence.length(); ++i)
+            if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1])
+                return false;
+        return sentence.front() == sentence.back();
+    }
+};
