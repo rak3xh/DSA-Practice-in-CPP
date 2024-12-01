556e59c7e4ce8700595aec47f998c810b4057d85
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-12-01 21:53:49 +0530

    21:53 01 Dec

---
+++ All DSA POTD/Leetcode/01Dec.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// C++ 20 Solution using contains
+
+/*
+class Solution
+{
+public:
+    bool checkIfExist(vector<int> &arr)
+    {
+        unordered_set<int> seen;
+
+        for (const int a : arr)
+        {
+            if (seen.contains(a * 2) || a % 2 == 0 && seen.contains(a / 2))
+                return true;
+            seen.insert(a);
+        }
+
+        return false;
+    }
+};
+*/
+class Solution
+{
+public:
+    bool checkIfExist(vector<int> &arr)
+    {
+        unordered_set<int> seen;
+
+        for (const int a : arr)
+        {
+            if (seen.find(a * 2) != seen.end() || (a % 2 == 0 && seen.find(a / 2) != seen.end()))
+                return true;
+            seen.insert(a);
+        }
+
+        return false;
+    }
+};
