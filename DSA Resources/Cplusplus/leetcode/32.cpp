03a837cf7341131fbbdff7ea1a8f8f66a7419420
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-12-01 22:03:13 +0530

    22:03 01 Dec

---
+++ DSA Resources/Cplusplus/leetcode/32.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int longestValidParentheses(string s)
+    {
+        string s2 = ")" + s;
+        // dp[i] := the length of the longest valid parentheses in the substring
+        // s2[1..i]
+        vector<int> dp(s2.length(), 0);
+
+        for (int i = 1; i < s2.length(); ++i)
+            if (s2[i] == ')' && s2[i - dp[i - 1] - 1] == '(')
+                dp[i] = dp[i - 1] + dp[i - dp[i - 1] - 2] + 2;
+
+        return *max_element(dp.begin(), dp.end());
+    }
+};
