3ff73ee04b6958bceba5607ac1c844b8012f5152
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-12-08 13:23:13 +0530

    13:23 08 Dec

--- All DSA POTD/Leetcode/08Dec.cpp
+++ All DSA POTD/Leetcode/08Dec.cpp
@@ -40,3 +40,53 @@ public:
     }
 };
 */
+
+// C++ 14 Solution
+struct Event
+{
+    int time;
+    int value;
+    bool isStart;
+
+    Event(int t, int v, bool s) : time(t), value(v), isStart(s) {}
+};
+
+class Solution
+{
+public:
+    int maxTwoEvents(vector<vector<int>> &events)
+    {
+        int ans = 0;
+        int maxValue = 0;
+        vector<Event> evts;
+
+        // Convert events into Event objects
+        for (const vector<int> &event : events)
+        {
+            const int start = event[0];
+            const int end = event[1];
+            const int value = event[2];
+            evts.emplace_back(start, value, true);
+            evts.emplace_back(end + 1, value, false);
+        }
+
+        // Sort events based on time and isStart flag
+        sort(evts.begin(), evts.end(), [](const Event &a, const Event &b)
+             { return a.time == b.time ? a.isStart < b.isStart : a.time < b.time; });
+
+        // Traverse through the sorted events
+        for (const Event &e : evts)
+        {
+            if (e.isStart)
+            {
+                ans = max(ans, e.value + maxValue);
+            }
+            else
+            {
+                maxValue = max(maxValue, e.value);
+            }
+        }
+
+        return ans;
+    }
+};
