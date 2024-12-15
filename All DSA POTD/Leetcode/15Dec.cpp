7e26ed3d2c995e3c771e99e8e32b8b3c855d68a3
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-12-15 13:54:53 +0530

    13:54 15 Dec

--- All DSA POTD/Leetcode/15Dec.cpp
+++ All DSA POTD/Leetcode/15Dec.cpp
@@ -1,6 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+    double maxAverageRatio(vector<vector<int>> &classes, int extraStudents)
+    {
+        // Priority queue to store tuples in descending order of extra pass ratio
+        priority_queue<tuple<double, int, int>> maxHeap;
+
+        for (const vector<int> &c : classes)
+        {
+            const int pass = c[0];
+            const int total = c[1];
+            maxHeap.emplace(extraPassRatio(pass, total), pass, total);
+        }
+
+        for (int i = 0; i < extraStudents; ++i)
+        {
+            double extraRatio;
+            int pass, total;
+            tie(extraRatio, pass, total) = maxHeap.top();
+            maxHeap.pop();
+            maxHeap.emplace(extraPassRatio(pass + 1, total + 1), pass + 1, total + 1);
+        }
+
+        double ratioSum = 0.0;
+
+        while (!maxHeap.empty())
+        {
+            double extraRatio;
+            int pass, total;
+            tie(extraRatio, pass, total) = maxHeap.top();
+            maxHeap.pop();
+            ratioSum += pass / static_cast<double>(total);
+        }
+
+        return ratioSum / classes.size();
+    }
+
+private:
+    // Returns the extra pass ratio if a brilliant student joins.
+    double extraPassRatio(int pass, int total)
+    {
+        return (pass + 1) / static_cast<double>(total + 1) -
+               pass / static_cast<double>(total);
+    }
+};
+
 // C++ 20 Solution
 
 /*
