88b2943617015c71b678d835c0119782916ab4fa
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-12-16 22:33:17 +0530

    22:33 16 Dec

--- All DSA POTD/Leetcode/16Dec.cpp
+++ All DSA POTD/Leetcode/16Dec.cpp
@@ -1,5 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// C++ 14 Solution
+class Solution
+{
+public:
+    vector<int> getFinalState(vector<int> &nums, int k, int multiplier)
+    {
+        vector<int> ans(nums.size());
+        using P = pair<int, int>; // (nums[i], i)
+        priority_queue<P, vector<P>, greater<P>> minHeap;
+
+        for (int i = 0; i < nums.size(); ++i)
+            minHeap.emplace(nums[i], i);
+
+        while (k-- > 0)
+        {
+            auto topElement = minHeap.top();
+            minHeap.pop();
+            int num = topElement.first;
+            int i = topElement.second;
+            minHeap.emplace(num * multiplier, i);
+        }
+
+        while (!minHeap.empty())
+        {
+            auto topElement = minHeap.top();
+            minHeap.pop();
+            int num = topElement.first;
+            int i = topElement.second;
+            ans[i] = num;
+        }
+
+        return ans;
+    }
+};
+
 // C++ 20 Solution
 /*
 class Solution
