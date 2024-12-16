a20c8f3d02644a9d7ee87a250b1d6f88530aee3a
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-12-16 22:31:06 +0530

    22:31 16 Dec

---
+++ All DSA POTD/GFG/16Dec.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int kthElement(vector<int> &a, vector<int> &b, int k)
+    {
+        // code here
+        a.insert(a.end(), b.begin(), b.end());
+        sort(a.begin(), a.end());
+        return a[k - 1];
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        int n, m, k;
+        cin >> k;
+        cin.ignore();
+        string input;
+        int num;
+        vector<int> a, b;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num)
+        {
+            a.push_back(num);
+        }
+
+        getline(cin, input);
+        stringstream s3(input);
+        while (s3 >> num)
+        {
+            b.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.kthElement(a, b, k) << endl
+             << "~\n";
+    }
+    return 0;
+}
