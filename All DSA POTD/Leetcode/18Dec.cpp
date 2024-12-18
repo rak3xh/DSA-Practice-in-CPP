e7d991fa186dcbaaa27c06031bd8016aea32f8de
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-12-18 20:41:49 +0530

    20:41 18 Dec

---
+++ All DSA POTD/Leetcode/18Dec.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> finalPrices(vector<int> &prices)
+    {
+        vector<int> ans{prices};
+        stack<int> stack;
+
+        for (int j = 0; j < prices.size(); ++j)
+        {
+            // stack[-1] : = i in the problem description.
+            while (!stack.empty() && prices[j] <= prices[stack.top()])
+                ans[stack.top()] -= prices[j], stack.pop();
+            stack.push(j);
+        }
+
+        return ans;
+    }
+};
