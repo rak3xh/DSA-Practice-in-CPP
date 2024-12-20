5737c04ff41f5ee631fd94fe431eb854ae518e57
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-12-21 00:17:03 +0530

    00:16 21 Dec

---
+++ DSA Resources/Cplusplus/sorting/mergeSort.cpp
@@ -0,0 +1,104 @@
+/**
+ * @file mergeSort.cpp
+ * @brief Implementation of Merge Sort algorithm using vectors and user input.
+ *
+ * This program sorts an array of integers using the Merge Sort algorithm.
+ * The user is prompted to enter the number of elements and the elements themselves.
+ * The sorted array is then displayed.
+ *
+ * Time Complexity:
+ * - Best case: O(n log n)
+ * - Average case: O(n log n)
+ * - Worst case: O(n log n)
+ *
+ * Space Complexity:
+ * - O(n) due to the use of additional vectors for merging.
+ *
+ * @author [rak3xh]
+ * @date [21 Dec 20245]
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void merge(vector<int> &arr, int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; ++i)
+        L[i] = arr[left + i];
+    for (int i = 0; i < n2; ++i)
+        R[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            ++i;
+        }
+        else
+        {
+            arr[k] = R[j];
+            ++j;
+        }
+        ++k;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        ++i;
+        ++k;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        ++j;
+        ++k;
+    }
+}
+
+void mergeSort(vector<int> &arr, int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
