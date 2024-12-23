689e291d7c5833874b996422b20c7f9e3aa36b9a
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-12-23 19:39:16 +0530

    19:39 23 Dec

--- All DSA POTD/Leetcode/23Dec.cpp
+++ All DSA POTD/Leetcode/23Dec.cpp
@@ -1,3 +1,65 @@
+// C++ 14 Solution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int minimumOperations(TreeNode *root)
+    {
+        int ans = 0;
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            vector<int> vals;
+            vector<int> ids(q.size());
+            for (int sz = q.size(); sz > 0; --sz)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                vals.push_back(node->val);
+                if (node->left != nullptr)
+                    q.push(node->left);
+                if (node->right != nullptr)
+                    q.push(node->right);
+            }
+
+            // Initialize the ids based on the order of vals.
+            iota(ids.begin(), ids.end(), 0);
+
+            // Sort ids based on the corresponding values in vals.
+            sort(ids.begin(), ids.end(), [&vals](int i, int j)
+                 { return vals[i] < vals[j]; });
+
+            // Count the number of swaps needed to sort ids.
+            for (int i = 0; i < ids.size(); ++i)
+            {
+                while (ids[i] != i)
+                {
+                    swap(ids[i], ids[ids[i]]);
+                    ++ans;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
 // C+ 20 Solution
 
 /*
