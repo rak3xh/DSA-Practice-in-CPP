cd4a41a2ed60ff32f6cf2b30650baab26490e8ad
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-12-27 21:02:59 +0530

    21:02 27 Dec

---
+++ All DSA POTD/Leetcode/27Dec.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxScoreSightseeingPair(vector<int> &values)
+    {
+        int ans = 0;
+        int bestPrev = 0;
+
+        for (const int value : values)
+        {
+            ans = max(ans, value + bestPrev);
+            bestPrev = max(bestPrev, value) - 1;
+        }
+
+        return ans;
+    }
+};
