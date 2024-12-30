31824337af61b1441bb5cd578bfa0e1d732aa850
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-12-31 00:58:43 +0530

    00:58 31 Dec

---
+++ DSA Resources/Cplusplus/leetcode/2363.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Sloution
+{
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>> &items1, vector<vector<int>> &items2)
+    {
+        constexpr int kMax = 1000;
+        vector<vector<int>> ans;
+        vector<int> count(kMax + 1);
+
+        for (const vector<int> &item : items1)
+            count[item[0]] += item[1];
+
+        for (const vector<int> &item : items2)
+            count[item[0]] += item[1];
+
+        for (int i = 1; i <= kMax; ++i)
+            if (count[i])
+                ans.push_back({i, count[i]});
+
+        return ans;
+    }
+};
