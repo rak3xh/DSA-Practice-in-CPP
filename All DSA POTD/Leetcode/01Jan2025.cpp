19352faf5291b99157955a613f5692711187b3b4
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-01-01 11:07:50 +0530

    11:07 01 Jan

--- All DSA POTD/Leetcode/01Jan2025.cpp
+++ All DSA POTD/Leetcode/01Jan2025.cpp
@@ -1,5 +1,30 @@
 // C++ 14 Solution
 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxScore(string s)
+    {
+        int ans = 0;
+        int zeros = 0;
+        int ones = count(s.begin(), s.end(), '1'); // Replaced ranges::count with std::count
+
+        for (int i = 0; i + 1 < s.size(); ++i)
+        {
+            if (s[i] == '0')
+                ++zeros;
+            else
+                --ones;
+            ans = max(ans, zeros + ones);
+        }
+
+        return ans;
+    }
+};
+
 // C++ 20 Solution
 
 /*
