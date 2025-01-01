3b4bb4a50644d9b303bb72786c49ff563d435b0e
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-01-01 11:03:15 +0530

    11:03 01 Jan

---
+++ All DSA POTD/GFG/01Jan2025.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    vector<vector<string>> anagrams(vector<string> &arr)
+    {
+        // code here
+        vector<vector<string>> ans;
+        map<vector<int>, vector<string>> mp;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            string str = arr[i];
+            vector<int> freq(26, 0);
+            for (auto x : str)
+                freq[x - 'a']++;
+
+            if (mp.find(freq) != mp.end())
+            {
+                mp[freq].push_back(str);
+            }
+            else
+                mp[freq] = {str};
+        }
+        for (auto x : mp)
+            ans.push_back(x.second);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+
+        vector<string> arr;
+        string input_line;
+        getline(cin, input_line);
+        stringstream ss(input_line);
+        string word;
+        while (ss >> word)
+        {
+            arr.push_back(word);
+        }
+
+        Solution ob;
+        vector<vector<string>> result = ob.anagrams(arr);
+        sort(result.begin(), result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for (int j = 0; j < result[i].size(); j++)
+            {
+                cout << result[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
