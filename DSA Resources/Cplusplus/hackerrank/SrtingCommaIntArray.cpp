4c392a2b78d9ab7f6b767bf19deacb5b86ee4705
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-01-08 23:32:46 +0530

    23:32 08 Jan

---
+++ DSA Resources/Cplusplus/hackerrank/SrtingCommaIntArray.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> parseInts(string str)
+{
+    vector<int> ans;
+    string input;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == ',')
+        {
+            ans.push_back(stoi(input));
+            input = "";
+        }
+        else
+        {
+            input += str[i];
+        }
+        ans.push_back(stoi(input));
+        return ans;
+    }
+
+    return ans;
+}
+int main()
+{
+    string str;
+    getline(cin, str);
+    vector<int> ans = parseInts(str);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << "\n";
+    }
+}
