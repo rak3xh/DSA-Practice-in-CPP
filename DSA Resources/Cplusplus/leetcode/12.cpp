8c767fbbf0dfec9e65e97c3a1f251e85e337d3b3
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-01-08 23:51:51 +0530

    23:51 08 Jan

--- DSA Resources/Cplusplus/leetcode/12.cpp
+++ DSA Resources/Cplusplus/leetcode/12.cpp
@@ -1,3 +1,35 @@
+// C++ 14 Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string intToRoman(int num)
+    {
+        const vector<pair<int, string>> valueSymbols{
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+        string ans;
+
+        for (const auto &pair : valueSymbols)
+        {
+            int value = pair.first;
+            const string &symbol = pair.second;
+
+            if (num == 0)
+                break;
+
+            while (num >= value)
+            {
+                num -= value;
+                ans += symbol;
+            }
+        }
+
+        return ans;
+    }
+};
+
 // C++ 20 Solution
 
 /*
