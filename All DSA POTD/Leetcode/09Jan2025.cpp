33ebaae24d8e5ad007f7f36b1b038644646af3ba
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-01-09 19:57:37 +0530

    19:57 09 Jan

---
+++ All DSA POTD/Leetcode/09Jan2025.cpp
@@ -0,0 +1,14 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int prefixCount(vector<string> &words, string pref)
+    {
+        return ranges::count_if(
+            words, [&](const string &word)
+            { return word.find(pref) == 0; });
+    }
+};
