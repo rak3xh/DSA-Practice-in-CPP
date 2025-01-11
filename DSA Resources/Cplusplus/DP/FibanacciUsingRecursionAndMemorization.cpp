aaf36cbc5d531488c6461075edd608f0f70e73cf
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-01-11 23:48:38 +0530

    Update: DP Series

---
+++ DSA Resources/Cplusplus/DP/FibanacciUsingRecursionAndMemorization.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <conio.h>
+using namespace std;
+int fib(int n, vector<int> &dp) // Recursion with Memorization
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    if (dp[n] != -1)
+    {
+        return dp[n];
+    }
+
+    dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
+
+    return dp[n];
+}
+int nthFibonacci(int n)
+{
+    vector<int> dp(n + 1, -1);
+    return fib(n, dp);
+}
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << nthFibonacci(n);
+    getch();
+}
