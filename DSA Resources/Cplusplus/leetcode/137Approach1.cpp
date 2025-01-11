d1063bd516d2fe21cecbb1fcd7f1718a05018b38
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-01-11 21:51:28 +0530

    21:51 11 Jan

---
+++ DSA Resources/Cplusplus/leetcode/137Approach1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int ones = 0;
+        int twos = 0;
+
+        for (const int num : nums)
+        {
+            ones ^= (num & ~twos);
+            twos ^= (num & ~ones);
+        }
+
+        return ones;
+    }
+};
