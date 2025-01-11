cc57381f153eeef5f084c7685715438a25092acd
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-01-11 23:44:18 +0530

    23:44 11 Jan

---
+++ DSA Resources/Cplusplus/GFG/FibonacciNthTermUsingDP.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fib(int n, vector<int> &dp)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    if (dp[n] != -1)
+    {
+        return dp[n];
+    }
+
+    dp[n] = fib(n - 1, dp) + fib(n + 1, dp);
+
+    return dp[n];
+}
+int nthFibonacci(int n)
+{
+    vector<int> dp(n + 1, -1);
+    return fib(n, dp);
+}
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << nthFibonacci(n);
+}
