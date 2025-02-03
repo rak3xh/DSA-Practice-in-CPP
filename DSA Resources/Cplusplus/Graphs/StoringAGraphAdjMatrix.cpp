9da357416b82b4159fbf9f2dcaca3baeffb62bc1
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-03 23:01:59 +0530

    23:01 03 Feb

--- DSA Resources/Cplusplus/Graphs/StoringAGraphAdjMatrix.cpp
+++ DSA Resources/Cplusplus/Graphs/StoringAGraphAdjMatrix.cpp
@@ -38,6 +38,7 @@ int main()
 */
 // Storing a Directed Graph
 // Space Complexity=O(E)
+/*
 int main()
 {
     int n, m;
@@ -48,6 +49,25 @@ int main()
         int u, v;
         cin >> u >> v;
         adj[u].push_back(v); // There is no bidirection edge
+    }
+    return 0;
+}
+*/
+// Storing a weighted graph in matrix
+// S.C=O(N^2)
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int adj[n + 1][m + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        int weight;
+        cin >> weight;
+        adj[u][v] = weight;
+        adj[v][u] = weight;
     }
     return 0;
 }
