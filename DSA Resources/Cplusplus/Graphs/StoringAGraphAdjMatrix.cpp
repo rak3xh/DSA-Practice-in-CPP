65938f5d8b2d7ea5e5eccacede7d0e62d52e0e5f
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-03 22:52:16 +0530

    22:52 03 Feb

--- DSA Resources/Cplusplus/Graphs/StoringAGraphAdjMatrix.cpp
+++ DSA Resources/Cplusplus/Graphs/StoringAGraphAdjMatrix.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+// Storing using Adjacency Matrix
+// Space complexity=O(n^2)
+/*
 int main()
 {
     int n, m;
@@ -11,6 +14,23 @@ int main()
         cin >> u >> v;
         adj[u][v] = 1;
         adj[v][u] = 1;
+    }
+    return 0;
+}
+*/
+// Storing using List
+// Space COmplexity=O(2E)
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> adj[n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
     return 0;
 }
