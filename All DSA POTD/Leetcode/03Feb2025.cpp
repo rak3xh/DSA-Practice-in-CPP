9043c9c963601ebcdb021e0b5bb144cb6a72d1b6
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-03 20:41:44 +0530

    20:41 03 Feb

---
+++ All DSA POTD/Leetcode/03Feb2025.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int longestMonotonicSubarray(vector<int> &nums)
+    {
+        int ans = 1;
+        int increasing = 1;
+        int decreasing = 1;
+
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                increasing += 1;
+                decreasing = 1;
+            }
+            else if (nums[i] < nums[i - 1])
+            {
+                decreasing += 1;
+                increasing = 1;
+            }
+            else
+            {
+                increasing = 1;
+                decreasing = 1;
+            }
+            ans = max({ans, increasing, decreasing});
+        }
+
+        return ans;
+    }
+};
