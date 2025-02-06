a3e0a0a871e375bcee41c711247b3da1e6f73090
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-06 21:16:40 +0530

    21:16 06 Feb

---
+++ All DSA POTD/Leetcode/06Feb2025.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int tupleSameProduct(vector<int> &nums)
+    {
+        int ans = 0;
+        unordered_map<int, int> count;
+
+        for (int i = 0; i < nums.size(); ++i)
+            for (int j = 0; j < i; ++j)
+                ans += count[nums[i] * nums[j]]++ * 8;
+
+        return ans;
+    }
+};
