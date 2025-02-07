401a7eb1b9aa385db21eef53f6481309121b46e2
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-07 20:41:34 +0530

    20:41 07 Feb

--- All DSA POTD/Leetcode/07Feb2025.cpp
+++ All DSA POTD/Leetcode/07Feb2025.cpp
@@ -1,3 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> queryResults(int limit, vector<vector<int>> &queries)
+    {
+        vector<int> ans;
+        unordered_map<int, int> ballToColor;
+        unordered_map<int, int> colorCount;
+
+        for (const vector<int> &query : queries)
+        {
+            int ball = query[0];
+            int color = query[1];
+
+            auto it = ballToColor.find(ball);
+            if (it != ballToColor.end()) // Check if ball is already assigned a color
+            {
+                int prevColor = it->second;
+                if (--colorCount[prevColor] == 0)
+                    colorCount.erase(prevColor);
+            }
+
+            ballToColor[ball] = color;
+            ++colorCount[color];
+            ans.push_back(colorCount.size());
+        }
+
+        return ans;
+    }
+};
+
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
