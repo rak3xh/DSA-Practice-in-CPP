d0bad179d4748f60079a537e6e0b8bfc17f45e38
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-08 10:15:26 +0530

    10:15 08 Feb

---
+++ All DSA POTD/Leetcode/08Feb2025.cpp
@@ -0,0 +1,41 @@
+// C++ 17 Solution
+#include <bits/stdc++.h>
+using namespace std;
+class NumberContainers
+{
+private:
+    unordered_map<int, int> d;
+    unordered_map<int, set<int>> g;
+
+public:
+    NumberContainers()
+    {
+    }
+
+    void change(int index, int number)
+    {
+        if (d.contains(index))
+        {
+            int oldNumber = d[index];
+            g[oldNumber].erase(index);
+            if (g[oldNumber].empty())
+            {
+                g.erase(oldNumber);
+            }
+        }
+        d[index] = number;
+        g[number].insert(index);
+    }
+
+    int find(int number)
+    {
+        return g.contains(number) ? *g[number].begin() : -1;
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
