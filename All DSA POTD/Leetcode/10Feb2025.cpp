a72d0de30aeb72800e52948bc00dab911ce8cfa0
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-10 21:07:37 +0530

    21:07 10 Feb

---
+++ All DSA POTD/Leetcode/10Feb2025.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string clearDigits(string s)
+    {
+        string ans;
+
+        for (const char c : s)
+            if (isdigit(c))
+                // Since `ans` only contains non-digit characters, popping the last
+                // character is equivalent to deleting the closest non-digit character.
+                ans.pop_back();
+            else
+                ans += c;
+
+        return ans;
+    }
+};
