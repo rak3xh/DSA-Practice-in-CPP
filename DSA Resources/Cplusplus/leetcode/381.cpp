9172591260f9d53b861bf54dc9a097619f10239d
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-13 21:28:36 +0530

    21:28 13 Feb

---
+++ DSA Resources/Cplusplus/leetcode/381.cpp
@@ -0,0 +1,55 @@
+//C++ 20 Solution
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+struct Item
+{
+    int val;
+    int indexInMap;
+};
+
+class RandomizedCollection
+{
+    public:
+    bool insert(int val)
+    {
+        valToIndices[val].push_back(items.size());
+        items.emplace_back(val, valToIndices[val].size() - 1);
+        return valToIndices[val].size() == 1;
+    }
+    
+    bool remove(int val)
+    {
+        if (!valToIndices.contains(val))
+        return false;
+        
+        const int index = valToIndices[val].back();
+        valToIndices[items.back().val][items.back().indexInMap] = index;
+        valToIndices[val].pop_back();
+        if (valToIndices[val].empty())
+        valToIndices.erase(val);
+        items[index] = items.back();
+        items.pop_back();
+        return true;
+    }
+    
+    int getRandom()
+    {
+        const int index = rand() % items.size();
+        return items[index].val;
+    }
+    
+    private:
+    unordered_map<int, vector<int>> valToIndices;
+    vector<Item> items;
+};
+
+
+//  Your RandomizedCollection object will be instantiated and called as such:
+//  RandomizedCollection* obj = new RandomizedCollection();
+//  bool param_1 = obj->insert(val);
+//  bool param_2 = obj->remove(val);
+//  int param_3 = obj->getRandom();
+
+*/
