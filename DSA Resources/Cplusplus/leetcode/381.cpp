e8d7fd084219641d7c593e60a4e95182092f11fe
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-13 21:29:13 +0530

    21:29 13 Feb

--- DSA Resources/Cplusplus/leetcode/381.cpp
+++ DSA Resources/Cplusplus/leetcode/381.cpp
@@ -1,4 +1,54 @@
-//C++ 20 Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item
+{
+    int val;
+    int indexInMap;
+};
+
+class RandomizedCollection
+{
+public:
+    bool insert(int val)
+    {
+        valToIndices[val].push_back(items.size());
+        items.emplace_back(Item{val, static_cast<int>(valToIndices[val].size() - 1)});
+        return valToIndices[val].size() == 1;
+    }
+
+    bool remove(int val)
+    {
+        auto it = valToIndices.find(val);
+        if (it == valToIndices.end())
+            return false;
+
+        int index = it->second.back();
+        it->second.pop_back();
+        if (it->second.empty())
+            valToIndices.erase(it);
+
+        if (index != items.size() - 1)
+        {
+            items[index] = items.back();
+            valToIndices[items[index].val][items[index].indexInMap] = index;
+        }
+
+        items.pop_back();
+        return true;
+    }
+
+    int getRandom()
+    {
+        return items[rand() % items.size()].val;
+    }
+
+private:
+    unordered_map<int, vector<int>> valToIndices;
+    vector<Item> items;
+};
+
+// C++ 20 Solution
 
 /*
 #include <bits/stdc++.h>
@@ -18,12 +68,12 @@ class RandomizedCollection
         items.emplace_back(val, valToIndices[val].size() - 1);
         return valToIndices[val].size() == 1;
     }
-    
+
     bool remove(int val)
     {
         if (!valToIndices.contains(val))
         return false;
-        
+
         const int index = valToIndices[val].back();
         valToIndices[items.back().val][items.back().indexInMap] = index;
         valToIndices[val].pop_back();
@@ -33,13 +83,13 @@ class RandomizedCollection
         items.pop_back();
         return true;
     }
-    
+
     int getRandom()
     {
         const int index = rand() % items.size();
         return items[index].val;
     }
-    
+
     private:
     unordered_map<int, vector<int>> valToIndices;
     vector<Item> items;
