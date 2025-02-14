51bdd2524994db6c85ccbe6334e683b0659d626b
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-14 21:34:12 +0530

    21:34 14 Fevb

---
+++ All DSA POTD/Leetcode/14Feb2025.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+class ProductOfNumbers
+{
+public:
+    ProductOfNumbers() : prefix{1} {}
+
+    void add(int num)
+    {
+        if (num == 0)
+            prefix = {1};
+        else
+            prefix.push_back(prefix.back() * num);
+    }
+
+    int getProduct(int k)
+    {
+        return k >= prefix.size() ? 0
+                                  : prefix.back() / prefix[prefix.size() - k - 1];
+    }
+
+private:
+    vector<int> prefix;
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
