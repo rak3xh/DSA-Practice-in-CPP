feb0991ba13abdf2f85bf81f466cda1782a9d967
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-15 21:34:26 +0530

    21:34 15 Feb

---
+++ All DSA POTD/GFG/15Feb2025.cpp
@@ -0,0 +1,173 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    // Constructor to initialize a new node
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// } Driver Code Ends
+/*
+// Tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    bool traverse(vector<Node *> &first, Node *root, Node *n)
+    {
+        if (!root)
+            return false;
+        first.push_back(root);
+        if (root == n)
+            return true;
+        if (traverse(first, root->left, n) || traverse(first, root->right, n))
+        {
+            return true;
+        }
+        first.pop_back();
+        return false;
+    }
+    Node *LCA(Node *root, Node *n1, Node *n2)
+    {
+        // code here
+        vector<Node *> first;
+        vector<Node *> second;
+        traverse(first, root, n1);
+        traverse(second, root, n2);
+
+        int i;
+        for (i = 0; i < first.size() && i < second.size(); i++)
+        {
+            if (first[i] != second[i])
+                break;
+        }
+        return first[i - 1];
+    }
+};
+
+//{ Driver Code Starts.
+
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+Node *searchOnTree(Node *root, int num)
+{
+    if (root == nullptr || root->data == num)
+        return root;
+    if (num < root->data)
+        return searchOnTree(root->left, num);
+    else
+        return searchOnTree(root->right, num);
+}
+
+int main()
+{
+
+    int t;
+    scanf("%d ", &t);
+    while (t--)
+    {
+        string s;
+        int l, h;
+        getline(cin, s);
+        scanf("%d ", &l);
+        scanf("%d ", &h);
+        Node *root = buildTree(s);
+        Node *n1 = searchOnTree(root, l);
+        Node *n2 = searchOnTree(root, h);
+        Solution sol;
+        int ans = sol.LCA(root, n1, n2)->data;
+        cout << ans << endl;
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
