7b5f0f5ddee8fa4b087f86f7fb04636dcf9cdd19
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-18 22:38:59 +0530

    22:38 18 Feb

---
+++ All DSA POTD/Leetcode/18Feb2025.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string smallestNumber(string pattern)
+    {
+        string ans;
+        stack<char> stack{{'1'}};
+
+        for (const char c : pattern)
+        {
+            char maxSorFar = stack.top();
+            if (c == 'I')
+                while (!stack.empty())
+                {
+                    maxSorFar = max(maxSorFar, stack.top());
+                    ans += stack.top(), stack.pop();
+                }
+            stack.push(maxSorFar + 1);
+        }
+
+        while (!stack.empty())
+            ans += stack.top(), stack.pop();
+
+        return ans;
+    }
+};
