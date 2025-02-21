ab2f7d5031e176e1fa970751d2f5bc64438c4e25
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-21 20:31:24 +0530

    20:31 21 Feb

--- All DSA POTD/Leetcode/22Feb2025.cpp
+++ All DSA POTD/Leetcode/22Feb2025.cpp
@@ -1,5 +1,54 @@
+// C++ 14 Solution
 
-// C++ 20 Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class FindElements
+{
+public:
+    FindElements(TreeNode *root)
+    {
+        dfs(root, 0);
+    }
+
+    bool find(int target)
+    {
+        return vals.find(target) != vals.end();
+    }
+
+private:
+    unordered_set<int> vals;
+
+    void dfs(TreeNode *root, int val)
+    {
+        if (root == nullptr)
+            return;
+
+        root->val = val;
+        vals.insert(val);
+        dfs(root->left, val * 2 + 1);
+        dfs(root->right, val * 2 + 2);
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
+
+//  C++ 20 Solution
 /*
 #include <bits/stdc++.h>
 using namespace std;
