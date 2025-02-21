d33a210cbdd85628cd7571b810f7135427e0dd86
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-21 20:25:50 +0530

    20:25 21 Feb

--- All DSA POTD/Leetcode/21Feb2025.cpp
+++ All DSA POTD/Leetcode/21Feb2025.cpp
@@ -1,3 +1,30 @@
+// C++ 14 Solution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string findDifferentBinaryString(vector<string> &nums)
+    {
+        const int bitSize = nums[0].length();
+        const int maxNum = 1 << bitSize;
+        unordered_set<int> numsSet;
+
+        for (const string &num : nums)
+            numsSet.insert(stoi(num, nullptr, 2));
+
+        for (int num = 0; num < maxNum; ++num)
+        {
+            if (numsSet.find(num) == numsSet.end())
+                return bitset<16>(num).to_string().substr(16 - bitSize);
+        }
+
+        throw runtime_error("No valid binary string found");
+    }
+};
+
 // C++ 20 Solution
 /*
 
