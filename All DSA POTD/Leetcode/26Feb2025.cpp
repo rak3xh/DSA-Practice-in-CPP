8f6bb1deb64e304eececa98cb0a8d261bc596c6e
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-26 20:54:38 +0530

    20:54 26 Feb

---
+++ All DSA POTD/Leetcode/26Feb2025.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxAbsoluteSum(vector<int> &nums)
+    {
+        int ans = INT_MIN;
+        int maxSum = 0;
+        int minSum = 0;
+
+        for (const int num : nums)
+        {
+            maxSum = max(num, maxSum + num);
+            minSum = min(num, minSum + num);
+            ans = max({ans, maxSum, -minSum});
+        }
+
+        return ans;
+    }
+};
