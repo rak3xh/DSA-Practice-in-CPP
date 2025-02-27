20bfb7ae00189b660c7ea44b5d76580a9b0b6c2e
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-02-27 22:52:19 +0530

    22:52 27 Feb

--- All DSA POTD/Leetcode/27Feb2025.cpp
+++ All DSA POTD/Leetcode/27Feb2025.cpp
@@ -1,3 +1,38 @@
+// C++ 14 Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int lenLongestFibSubseq(vector<int> &arr)
+    {
+        const int n = arr.size();
+        int ans = 0;
+        vector<vector<int>> dp(n, vector<int>(n, 2));
+        unordered_map<int, int> numToIndex;
+
+        for (int i = 0; i < n; ++i)
+            numToIndex[arr[i]] = i;
+
+        for (int j = 0; j < n; ++j)
+        {
+            for (int k = j + 1; k < n; ++k)
+            {
+                const int ai = arr[k] - arr[j];
+                if (ai < arr[j] && numToIndex.find(ai) != numToIndex.end())
+                {
+                    const int i = numToIndex[ai];
+                    dp[j][k] = dp[i][j] + 1;
+                    ans = max(ans, dp[j][k]);
+                }
+            }
+        }
+
+        return ans > 2 ? ans : 0;
+    }
+};
+
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
