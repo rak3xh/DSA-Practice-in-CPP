251ebf18123eca507ebcbb772a36c774277a4247
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-08 16:55:05 +0530

    16:55 08 Mar

---
+++ All DSA POTD/Leetcode/08Mar2025.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minimumRecolors(string blocks, int k)
+    {
+        int countB = 0;
+        int maxCountB = 0;
+
+        for (int i = 0; i < blocks.length(); ++i)
+        {
+            if (blocks[i] == 'B')
+                ++countB;
+            if (i >= k && blocks[i - k] == 'B')
+                --countB;
+            maxCountB = max(maxCountB, countB);
+        }
+
+        return k - maxCountB;
+    }
+};
