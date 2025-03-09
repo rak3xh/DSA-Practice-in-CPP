bbdb788102fd6906c6282a40bdc32588066c9df3
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-09 13:52:23 +0530

    13:52 09 MAr

---
+++ All DSA POTD/GFG/09Mar2025.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    int countPS(string &s)
+    {
+        // code here
+        unordered_map<int, int> m;
+        int n = s.size();
+        int c = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int l = i, r = i;
+            while (l >= 0 and r < n and s[l] == s[r])
+            {
+                if (r - l + 1 >= 2)
+                {
+                    if (m.find(l) != m.end() and m[l] != r)
+                        c++;
+                    else
+                        m[l] = r;
+                }
+                l--;
+                r++;
+            }
+            l = i, r = i + 1;
+            while (l >= 0 and r < n and s[l] == s[r])
+            {
+                if (r - l + 1 >= 2)
+                {
+                    if (m.find(l) != m.end() and m[l] != r)
+                        c++;
+                    else
+                        m[l] = r;
+                }
+                l--;
+                r++;
+            }
+        }
+        return c + m.size();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.countPS(s) << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
