d3c384ec3393147bf0a3bad1c3a5b875fe7fcf84
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-10 20:44:37 +0530

    20:44 10 Mar

--- All DSA POTD/Leetcode/10Mar2025.cpp
+++ All DSA POTD/Leetcode/10Mar2025.cpp
@@ -1,3 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    long long countOfSubstrings(string word, int k)
+    {
+        return substringsWithAtMost(word, k) - substringsWithAtMost(word, k - 1);
+    }
+
+private:
+    long substringsWithAtMost(const string &word, int k)
+    {
+        if (k == -1)
+            return 0;
+
+        long res = 0;
+        int vowels = 0;
+        int uniqueVowels = 0;
+        unordered_map<char, int> vowelLastSeen;
+
+        for (int l = 0, r = 0; r < word.length(); ++r)
+        {
+            if (isVowel(word[r]))
+            {
+                ++vowels;
+                if (vowelLastSeen.find(word[r]) == vowelLastSeen.end() || vowelLastSeen[word[r]] < l)
+                    ++uniqueVowels;
+                vowelLastSeen[word[r]] = r;
+            }
+            while (r - l + 1 - vowels > k)
+            {
+                if (isVowel(word[l]))
+                {
+                    --vowels;
+                    if (vowelLastSeen[word[l]] == l)
+                        --uniqueVowels;
+                }
+                ++l;
+            }
+            if (uniqueVowels == 5)
+                res += min({vowelLastSeen['a'], vowelLastSeen['e'], vowelLastSeen['i'],
+                            vowelLastSeen['o'], vowelLastSeen['u']}) -
+                       l + 1;
+        }
+
+        return res;
+    }
+
+    bool isVowel(char c)
+    {
+        static const unordered_set<char> kVowels = {'a', 'e', 'i', 'o', 'u'};
+        return kVowels.find(c) != kVowels.end();
+    }
+};
+
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
