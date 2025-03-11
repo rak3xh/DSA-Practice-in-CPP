ce9c54874f0474e8bd1115c0f9e89327d48cbe25
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-11 20:10:24 +0530

    20:10 1 Mar

---
+++ All DSA POTD/Leetcode/11Mar2025.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int numberOfSubstrings(string s)
+    {
+        int ans = 0;
+        vector<int> count(3);
+
+        int l = 0;
+        for (const char c : s)
+        {
+            ++count[c - 'a'];
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0)
+                --count[s[l++] - 'a'];
+            // s[0..r], s[1..r], ..., s[l - 1..r] are satified strings.
+            ans += l;
+        }
+
+        return ans;
+    }
+};
