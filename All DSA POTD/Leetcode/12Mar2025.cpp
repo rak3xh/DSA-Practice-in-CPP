adb2859e46207445769f9f754c7810233e9c4880
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-12 22:09:36 +0530

    22:09 12 Mar

--- All DSA POTD/Leetcode/12Mar2025.cpp
+++ All DSA POTD/Leetcode/12Mar2025.cpp
@@ -1,3 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maximumCount(vector<int> &nums)
+    {
+        return max(count_if(nums.begin(), nums.end(), [](int num)
+                            { return num > 0; }),
+                   count_if(nums.begin(), nums.end(), [](int num)
+                            { return num < 0; }));
+    }
+};
 // C++ 20 SOlution
 /*
 #include <bits/stdc++.h>
