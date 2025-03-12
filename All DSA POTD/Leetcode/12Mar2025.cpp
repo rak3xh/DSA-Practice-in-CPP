e2425e5f1c4342684684e87c4287f4a196d8a264
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-12 22:09:24 +0530

    22:09 12 MAr

--- All DSA POTD/Leetcode/12Mar2025.cpp
+++ All DSA POTD/Leetcode/12Mar2025.cpp
@@ -1,14 +1,16 @@
 // C++ 20 SOlution
+/*
 #include <bits/stdc++.h>
 using namespace std;
 class Solution
 {
-public:
+    public:
     int maximumCount(vector<int> &nums)
     {
         return max(ranges::count_if(nums, [](int num)
-                                    { return num > 0; }),
-                   ranges::count_if(nums, [](int num)
-                                    { return num < 0; }));
+        { return num > 0; }),
+        ranges::count_if(nums, [](int num)
+        { return num < 0; }));
     }
 };
+*/
