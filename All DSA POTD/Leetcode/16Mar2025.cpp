4e750ec0afe155f06143c30416d14d5ddde5567b
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-16 13:33:54 +0530

    13:33 16 Mar

--- All DSA POTD/Leetcode/16Mar2025.cpp
+++ All DSA POTD/Leetcode/16Mar2025.cpp
@@ -1,3 +1,37 @@
+// C++ 14 Solution
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    long long repairCars(vector<int> &ranks, int cars)
+    {
+        long l = 0;
+        long r = static_cast<long>(*min_element(ranks.begin(), ranks.end())) * cars * cars;
+
+        while (l < r)
+        {
+            const long m = (l + r) / 2;
+            if (numCarsFixed(ranks, m) >= cars)
+                r = m;
+            else
+                l = m + 1;
+        }
+
+        return l;
+    }
+
+private:
+    long numCarsFixed(const vector<int> &ranks, long minutes)
+    {
+        long carsFixed = 0;
+        //    r * n^2 = minutes
+        // -> n = sqrt(minutes / r)
+        for (const int rank : ranks)
+            carsFixed += sqrt(minutes / rank);
+        return carsFixed;
+    }
+};
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
@@ -9,7 +43,7 @@ class Solution
     {
         long l = 0;
         long r = static_cast<long>(ranges::min(ranks)) * cars * cars;
-        
+
         while (l < r)
         {
             const long m = (l + r) / 2;
@@ -18,10 +52,10 @@ class Solution
             else
             l = m + 1;
         }
-        
+
         return l;
     }
-    
+
     private:
     long numCarsFixed(const vector<int> &ranks, long minutes)
     {
