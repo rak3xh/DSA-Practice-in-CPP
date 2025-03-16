8a343de4b466389a45e80576dfb5d91902d9d0a9
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-16 13:33:07 +0530

    13:33 16 Mar

--- All DSA POTD/Leetcode/16Mar2025.cpp
+++ All DSA POTD/Leetcode/16Mar2025.cpp
@@ -1,33 +1,36 @@
+// C++ 20 Solution
+/*
 #include <bits/stdc++.h>
 using namespace std;
 class Solution
 {
-public:
+    public:
     long long repairCars(vector<int> &ranks, int cars)
     {
         long l = 0;
         long r = static_cast<long>(ranges::min(ranks)) * cars * cars;
-
+        
         while (l < r)
         {
             const long m = (l + r) / 2;
             if (numCarsFixed(ranks, m) >= cars)
-                r = m;
+            r = m;
             else
-                l = m + 1;
+            l = m + 1;
         }
-
+        
         return l;
     }
-
-private:
+    
+    private:
     long numCarsFixed(const vector<int> &ranks, long minutes)
     {
         long carsFixed = 0;
         //    r * n^2 = minutes
         // -> n = sqrt(minutes / r)
         for (const int rank : ranks)
-            carsFixed += sqrt(minutes / rank);
+        carsFixed += sqrt(minutes / rank);
         return carsFixed;
     }
 };
+*/
