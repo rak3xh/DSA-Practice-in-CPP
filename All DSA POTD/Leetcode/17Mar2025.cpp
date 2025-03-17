a74c2fa809195650357b3f674d25bf338a560e03
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-17 11:47:04 +0530

    11:46 17 Mar

---
+++ All DSA POTD/Leetcode/17Mar2025.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool divideArray(vector<int> &nums)
+    {
+        vector<int> count(501);
+
+        for (const int num : nums)
+            ++count[num];
+
+        return ranges::all_of(count, [](int c)
+                              { return c % 2 == 0; });
+    }
+};
