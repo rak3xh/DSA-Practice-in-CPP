2efbae9f485200eadcde745b9ed40b80c7f83948
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-17 11:48:36 +0530

    11:48 17 Mar

--- All DSA POTD/Leetcode/17Mar2025.cpp
+++ All DSA POTD/Leetcode/17Mar2025.cpp
@@ -1,3 +1,22 @@
+// C++ 14 Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool divideArray(vector<int> &nums)
+    {
+        vector<int> count(501, 0);
+
+        for (const int num : nums)
+            ++count[num];
+
+        return all_of(count.begin(), count.end(), [](int c)
+                      { return c % 2 == 0; });
+    }
+};
+
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
