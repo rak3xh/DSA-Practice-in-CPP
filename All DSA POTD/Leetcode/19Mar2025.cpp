cef8e8ec5b3cf095f95866fcaa3448b9b94239f1
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-19 21:31:08 +0530

    21:31 19 Mar

---
+++ All DSA POTD/Leetcode/19Mar2025.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums)
+    {
+        const int n = nums.size();
+        int ans = 0;
+
+        for (int i = 0; i + 2 < n; ++i)
+            if (nums[i] == 0)
+            {
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
+                ++ans;
+            }
+
+        return nums[n - 1] == 0 || nums[n - 2] == 0 ? -1 : ans;
+    }
+};
