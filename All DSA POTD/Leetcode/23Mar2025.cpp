0bfeb10a6bde0121f5be95a3451fd20672ef4692
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-23 12:51:24 +0530

    12:51 23 Mar

--- All DSA POTD/Leetcode/23Mar2025.cpp
+++ All DSA POTD/Leetcode/23Mar2025.cpp
@@ -1,3 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int countPaths(int n, vector<vector<int>> &roads)
+    {
+        vector<vector<pair<int, int>>> graph(n);
+
+        for (const vector<int> &road : roads)
+        {
+            int u = road[0];
+            int v = road[1];
+            int w = road[2];
+            graph[u].emplace_back(v, w);
+            graph[v].emplace_back(u, w);
+        }
+
+        return dijkstra(graph, 0, n - 1);
+    }
+
+private:
+    int dijkstra(const vector<vector<pair<int, int>>> &graph, int src, int dst)
+    {
+        const int kMod = 1000000007;
+        vector<long> ways(graph.size(), 0);
+        vector<long> dist(graph.size(), LONG_MAX);
+
+        ways[src] = 1;
+        dist[src] = 0;
+
+        using P = pair<long, int>; // (distance, node)
+        priority_queue<P, vector<P>, greater<P>> minHeap;
+        minHeap.emplace(0, src);
+
+        while (!minHeap.empty())
+        {
+            pair<long, int> top = minHeap.top();
+            minHeap.pop();
+            long d = top.first;
+            int u = top.second;
+
+            if (d > dist[u])
+                continue;
+
+            for (const auto &edge : graph[u])
+            {
+                int v = edge.first;
+                int w = edge.second;
+
+                if (d + w < dist[v])
+                {
+                    dist[v] = d + w;
+                    ways[v] = ways[u];
+                    minHeap.emplace(dist[v], v);
+                }
+                else if (d + w == dist[v])
+                {
+                    ways[v] = (ways[v] + ways[u]) % kMod;
+                }
+            }
+        }
+
+        return ways[dst];
+    }
+};
+
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
