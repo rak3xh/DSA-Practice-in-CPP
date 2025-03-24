74d177adcd5490f5822c66006aa7677bd3a7ce03
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-24 21:12:24 +0530

    21:12 24 Mar

--- All DSA POTD/Leetcode/24mar2025.cpp
+++ All DSA POTD/Leetcode/24mar2025.cpp
@@ -1,3 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int countDays(int days, vector<vector<int>> &meetings)
+    {
+        int freeDays = 0;
+        int prevEnd = 0;
+
+        sort(meetings.begin(), meetings.end());
+
+        for (const vector<int> &meeting : meetings)
+        {
+            const int start = meeting[0];
+            const int end = meeting[1];
+            if (start > prevEnd)
+                freeDays += start - prevEnd - 1;
+            prevEnd = max(prevEnd, end);
+        }
+
+        return freeDays + max(0, days - prevEnd);
+    }
+};
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
@@ -9,9 +33,9 @@ class Solution
     {
         int freeDays = 0;
         int prevEnd = 0;
-        
+
         ranges::sort(meetings);
-        
+
         for (const vector<int> &meeting : meetings)
         {
             const int start = meeting[0];
@@ -20,7 +44,7 @@ class Solution
             freeDays += start - prevEnd - 1;
             prevEnd = max(prevEnd, end);
         }
-        
+
         return freeDays + max(0, days - prevEnd);
     }
 };
