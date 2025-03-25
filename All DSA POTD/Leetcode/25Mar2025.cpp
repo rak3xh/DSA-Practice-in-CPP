46f913b26f91220ffc792886bab21b8fcf85e510
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-25 19:47:24 +0530

    19:47 25 Mar

--- All DSA POTD/Leetcode/25Mar2025.cpp
+++ All DSA POTD/Leetcode/25Mar2025.cpp
@@ -1,3 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool checkValidCuts(int n, vector<vector<int>> &rectangles)
+    {
+        vector<pair<int, int>> xs;
+        vector<pair<int, int>> ys;
+
+        for (const vector<int> &rectangle : rectangles)
+        {
+            const int startX = rectangle[0];
+            const int startY = rectangle[1];
+            const int endX = rectangle[2];
+            const int endY = rectangle[3];
+            xs.emplace_back(startX, endX);
+            ys.emplace_back(startY, endY);
+        }
+
+        return max(countMerged(xs), countMerged(ys)) >= 3;
+    }
+
+private:
+    int countMerged(vector<pair<int, int>> &intervals)
+    {
+        int count = 0;
+        int prevEnd = 0;
+
+        sort(intervals.begin(), intervals.end());
+
+        for (size_t i = 0; i < intervals.size(); ++i)
+        {
+            int start = intervals[i].first;
+            int eend = intervals[i].second;
+
+            if (start < prevEnd)
+            {
+                prevEnd = max(prevEnd, eend);
+            }
+            else
+            {
+                prevEnd = eend;
+                ++count;
+            }
+        }
+
+        return count;
+    }
+};
+
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
@@ -9,7 +61,7 @@ class Solution
     {
         vector<pair<int, int>> xs;
         vector<pair<int, int>> ys;
-        
+
         for (const vector<int> rectangles : rectangles)
         {
             const int startX = rectangles[0];
@@ -19,18 +71,18 @@ class Solution
             xs.emplace_back(startX, endX);
             ys.emplace_back(startY, endY);
         }
-        
+
         return max(countMerged(xs), countMerged(ys)) >= 3;
     }
-    
+
     private:
     int countMerged(vector<pair<int, int>> &intervals)
     {
         int count = 0;
         int prevEnd = 0;
-        
+
         sort(intervals.begin(), intervals.end());
-        
+
         for (const auto &[start, eend] : intervals)
         if (start < prevEnd)
         {
@@ -41,7 +93,7 @@ class Solution
             prevEnd = eend;
             ++count;
         }
-        
+
         return count;
     }
 };
