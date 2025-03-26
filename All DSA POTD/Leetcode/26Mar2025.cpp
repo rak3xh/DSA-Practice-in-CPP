3ef0b5d783da5e9d3f771a6f3d69fd5fa9178dea
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-26 19:51:02 +0530

    29:50 26 Mar

---
+++ All DSA POTD/Leetcode/26Mar2025.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minOperations(vector<vector<int>> &grid, int x)
+    {
+        vector<int> arr;
+        for (const vector<int> &row : grid)
+            arr.insert(arr.end(), row.begin(), row.end());
+
+        for (int a : arr)
+        {
+            if ((a - arr[0]) % x != 0)
+                return -1;
+        }
+
+        int ans = 0;
+
+        nth_element(arr.begin(), arr.begin() + arr.size() / 2, arr.end());
+        int median = arr[arr.size() / 2];
+
+        for (int a : arr)
+            ans += abs(a - median) / x;
+
+        return ans;
+    }
+};
