baa0a61121eba0be0e97e63e571c93cb1f5d6efe
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-28 21:30:56 +0530

    21:30 28 MAr

--- All DSA POTD/Leetcode/28Mar2025.cpp
+++ All DSA POTD/Leetcode/28Mar2025.cpp
@@ -1,3 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct IndexedQuery
+{
+    int queryIndex;
+    int query;
+};
+
+struct T
+{
+    int i;
+    int j;
+    int val;
+};
+
+class Solution
+{
+public:
+    vector<int> maxPoints(vector<vector<int>> &grid, vector<int> &queries)
+    {
+        constexpr int kDirs[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> ans(queries.size());
+
+        auto compare = [](const T &a, const T &b)
+        { return a.val > b.val; };
+        priority_queue<T, vector<T>, decltype(compare)> minHeap(compare);
+        vector<vector<bool>> seen(m, vector<bool>(n, false));
+
+        minHeap.push({0, 0, grid[0][0]});
+        seen[0][0] = true;
+        int accumulate = 0;
+
+        vector<IndexedQuery> indexedQueries = getIndexedQueries(queries);
+        for (const auto &indexedQuery : indexedQueries)
+        {
+            int queryIndex = indexedQuery.queryIndex;
+            int query = indexedQuery.query;
+
+            while (!minHeap.empty())
+            {
+                T top = minHeap.top();
+                if (top.val >= query)
+                {
+                    break;
+                }
+                minHeap.pop();
+                ++accumulate;
+                for (const auto &dir : kDirs)
+                {
+                    int x = top.i + dir[0];
+                    int y = top.j + dir[1];
+                    if (x < 0 || x >= m || y < 0 || y >= n || seen[x][y])
+                    {
+                        continue;
+                    }
+                    minHeap.push({x, y, grid[x][y]});
+                    seen[x][y] = true;
+                }
+            }
+            ans[queryIndex] = accumulate;
+        }
+
+        return ans;
+    }
+
+private:
+    vector<IndexedQuery> getIndexedQueries(const vector<int> &queries)
+    {
+        vector<IndexedQuery> indexedQueries;
+        for (int i = 0; i < queries.size(); ++i)
+        {
+            indexedQueries.push_back({i, queries[i]});
+        }
+        sort(indexedQueries.begin(), indexedQueries.end(), [](const IndexedQuery &a, const IndexedQuery &b)
+             { return a.query < b.query; });
+        return indexedQueries;
+    }
+};
 // C++ 20 Solution
 /*
 struct IndexedQuery
