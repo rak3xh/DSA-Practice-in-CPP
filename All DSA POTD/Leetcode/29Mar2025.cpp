f567ad4209bacbffc875d4a0d2f0a4d95235907e
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-03-29 13:11:04 +0530

    13:10 29 Mar

--- All DSA POTD/Leetcode/29Mar2025.cpp
+++ All DSA POTD/Leetcode/29Mar2025.cpp
@@ -1,4 +1,112 @@
 // C++ 14 Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maximumScore(vector<int> &nums, int k)
+    {
+        const int n = nums.size();
+        const int mx = *max_element(nums.begin(), nums.end());
+        const vector<int> minPrimeFactors = sieveEratosthenes(mx + 1);
+        const vector<int> primeScores = getPrimeScores(nums, minPrimeFactors);
+        int ans = 1;
+        vector<int> left(n, -1), right(n, n);
+        stack<int> s;
+
+        for (int i = n - 1; i >= 0; --i)
+        {
+            while (!s.empty() && primeScores[s.top()] <= primeScores[i])
+            {
+                left[s.top()] = i;
+                s.pop();
+            }
+            s.push(i);
+        }
+
+        while (!s.empty())
+            s.pop();
+
+        for (int i = 0; i < n; ++i)
+        {
+            while (!s.empty() && primeScores[s.top()] < primeScores[i])
+            {
+                right[s.top()] = i;
+                s.pop();
+            }
+            s.push(i);
+        }
+
+        vector<pair<int, int>> numAndIndexes;
+        for (int i = 0; i < n; ++i)
+            numAndIndexes.emplace_back(nums[i], i);
+
+        sort(numAndIndexes.begin(), numAndIndexes.end(), [](const pair<int, int> &a, const pair<int, int> &b)
+             { return a.first == b.first ? a.second < b.second : a.first > b.first; });
+
+        for (const auto &p : numAndIndexes)
+        {
+            int num = p.first, i = p.second;
+            long rangeCount = static_cast<long>(i - left[i]) * (right[i] - i);
+            long actualCount = min(rangeCount, static_cast<long>(k));
+            k -= actualCount;
+            ans = static_cast<long>(ans) * modPow(num, actualCount) % kMod;
+        }
+
+        return ans;
+    }
+
+private:
+    static constexpr int kMod = 1'000'000'007;
+
+    long modPow(long x, long n)
+    {
+        if (n == 0)
+            return 1;
+        if (n % 2 == 1)
+            return x * modPow(x % kMod, n - 1) % kMod;
+        return modPow(x * x % kMod, n / 2) % kMod;
+    }
+
+    vector<int> sieveEratosthenes(int n)
+    {
+        vector<int> minPrimeFactors(n + 1);
+        for (int i = 2; i <= n; ++i)
+            minPrimeFactors[i] = i;
+        for (int i = 2; i * i <= n; ++i)
+        {
+            if (minPrimeFactors[i] == i)
+            {
+                for (int j = i * i; j <= n; j += i)
+                    if (minPrimeFactors[j] == j)
+                        minPrimeFactors[j] = i;
+            }
+        }
+        return minPrimeFactors;
+    }
+
+    vector<int> getPrimeScores(const vector<int> &nums, const vector<int> &minPrimeFactors)
+    {
+        vector<int> primeScores;
+        for (const int num : nums)
+            primeScores.push_back(getPrimeScore(num, minPrimeFactors));
+        return primeScores;
+    }
+
+    int getPrimeScore(int num, const vector<int> &minPrimeFactors)
+    {
+        unordered_set<int> primeFactors;
+        while (num > 1)
+        {
+            int divisor = minPrimeFactors[num];
+            primeFactors.insert(divisor);
+            while (num % divisor == 0)
+                num /= divisor;
+        }
+        return primeFactors.size();
+    }
+};
 
 //  C++ 20 Solition
 /*
