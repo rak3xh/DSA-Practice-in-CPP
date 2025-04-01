9b875796a0bfd5468ebf8d14dd1a69c9dc8f28da
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-04-01 20:53:17 +0530

    20:53 01 Apr

---
+++ All DSA POTD/GFG/01Apr2025.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    int n;
+    vector<bool> vis;
+    vector<int> ans;
+
+    void dfs(int node, vector<vector<int>> &adj)
+    {
+        vis[node] = 1;
+        ans.push_back(node);
+        for (auto &ngbr : adj[node])
+        {
+            if (!vis[ngbr])
+                dfs(ngbr, adj);
+        }
+    }
+    vector<int> dfs(vector<vector<int>> &adj)
+    {
+        // Code here
+        n = adj.size();
+        vis.resize(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            if (!vis[i])
+                dfs(i, adj);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--)
+    {
+        int V;
+        cin >> V;
+        cin.ignore();
+        vector<vector<int>> adj(
+            V); // Use vector of vectors instead of array of vectors.
+
+        for (int i = 0; i < V; i++)
+        {
+            string input;
+            getline(cin, input);
+            int num;
+            vector<int> node;
+            stringstream ss(input);
+            while (ss >> num)
+            {
+                node.push_back(num);
+            }
+            adj[i] = node;
+        }
+
+        Solution obj;
+        vector<int> ans = obj.dfs(adj);
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
