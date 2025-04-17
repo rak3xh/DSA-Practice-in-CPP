e7db5db69eb517718ffbab7eb6645e2450290f92
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-04-17 20:07:27 +0530

    20:07 17 Apr

--- All DSA POTD/Leetcode/17Apr2025.cpp
+++ All DSA POTD/Leetcode/17Apr2025.cpp
@@ -1,17 +1,17 @@
 // C++ 14 Solution
-
+/*
 #include <bits/stdc++.h>
 using namespace std;
 class Solution
 {
 public:
-    int countPairs(vector<int> &nums, int k)
-    {
+int countPairs(vector<int> &nums, int k)
+{
         int ans = 0;
         unordered_map<int, vector<int>> numToIndices;
 
         for (int i = 0; i < nums.size(); ++i)
-            numToIndices[nums[i]].push_back(i);
+        numToIndices[nums[i]].push_back(i);
 
         for (const auto &[_, indices] : numToIndices)
         {
@@ -20,8 +20,8 @@ public:
             {
                 const int gcd_i = gcd(i, k);
                 for (const auto &[gcd_j, count] : gcds)
-                    if (gcd_i * gcd_j % k == 0)
-                        ans += count;
+                if (gcd_i * gcd_j % k == 0)
+                ans += count;
                 ++gcds[gcd_i];
             }
         }
@@ -29,3 +29,4 @@ public:
         return ans;
     }
 };
+*/
