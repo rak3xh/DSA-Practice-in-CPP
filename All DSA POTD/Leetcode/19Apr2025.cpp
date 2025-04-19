536b4146f9d6bb0a310333ef4e949902ed97d95a
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-04-19 12:41:20 +0530

    12:41 19 Apr

--- All DSA POTD/Leetcode/19Apr2025.cpp
+++ All DSA POTD/Leetcode/19Apr2025.cpp
@@ -1,3 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    long long countFairPairs(vector<int> &nums, int lower, int upper)
+    {
+        // nums[i] + nums[j] == nums[j] + nums[i], so the condition that i < j
+        // degrades to i != j and we can sort the array.
+        sort(nums.begin(), nums.end());
+        return countLess(nums, upper) - countLess(nums, lower - 1);
+    }
+
+private:
+    long countLess(const vector<int> &nums, int sum)
+    {
+        long res = 0;
+        for (int i = 0, j = nums.size() - 1; i < j; ++i)
+        {
+            while (i < j && nums[i] + nums[j] > sum)
+                --j;
+            res += j - i;
+        }
+        return res;
+    }
+};
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
