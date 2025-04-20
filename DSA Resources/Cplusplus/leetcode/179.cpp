6e3211a70af5650b57a7a438cfd2543d75f8e384
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-04-20 22:38:28 +0530

    22:38 20 Apr

--- DSA Resources/Cplusplus/leetcode/179.cpp
+++ DSA Resources/Cplusplus/leetcode/179.cpp
@@ -1,3 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int ladderLength(string beginWord, string endWord, vector<string> &wordList)
+    {
+        unordered_set<string> wordSet(wordList.begin(), wordList.end());
+        if (wordSet.find(endWord) == wordSet.end())
+            return 0;
+
+        queue<string> q;
+        q.push(beginWord);
+
+        for (int step = 1; !q.empty(); ++step)
+        {
+            int sz = q.size();
+            for (int i = 0; i < sz; ++i)
+            {
+                string word = q.front();
+                q.pop();
+
+                for (int j = 0; j < word.length(); ++j)
+                {
+                    char cache = word[j];
+                    for (char c = 'a'; c <= 'z'; ++c)
+                    {
+                        word[j] = c;
+                        if (word == endWord)
+                            return step + 1;
+                        if (wordSet.find(word) != wordSet.end())
+                        {
+                            q.push(word);
+                            wordSet.erase(word);
+                        }
+                    }
+                    word[j] = cache;
+                }
+            }
+        }
+
+        return 0;
+    }
+};
+
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
