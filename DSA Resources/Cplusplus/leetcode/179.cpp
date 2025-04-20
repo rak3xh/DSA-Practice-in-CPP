995d944eb65bc75bd96543a74c77b50339825b20
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-04-20 22:38:05 +0530

    22:37 20 Apr

--- DSA Resources/Cplusplus/leetcode/179.cpp
+++ DSA Resources/Cplusplus/leetcode/179.cpp
@@ -1,40 +1,42 @@
 // C++ 20 Solution
+/*
 #include <bits/stdc++.h>
 using namespace std;
 class Solution
 {
-public:
+    public:
     int ladderLength(string beginWord, string endWord, vector<string> &wordList)
     {
         unordered_set<string> wordSet(wordList.begin(), wordList.end());
         if (!wordSet.contains(endWord))
-            return 0;
+        return 0;
 
         queue<string> q{{beginWord}};
 
         for (int step = 1; !q.empty(); ++step)
-            for (int sz = q.size(); sz > 0; --sz)
+        for (int sz = q.size(); sz > 0; --sz)
+        {
+            string word = q.front();
+            q.pop();
+            for (int i = 0; i < word.length(); ++i)
             {
-                string word = q.front();
-                q.pop();
-                for (int i = 0; i < word.length(); ++i)
+                const char cache = word[i];
+                for (char c = 'a'; c <= 'z'; ++c)
                 {
-                    const char cache = word[i];
-                    for (char c = 'a'; c <= 'z'; ++c)
+                    word[i] = c;
+                    if (word == endWord)
+                    return step + 1;
+                    if (wordSet.contains(word))
                     {
-                        word[i] = c;
-                        if (word == endWord)
-                            return step + 1;
-                        if (wordSet.contains(word))
-                        {
-                            q.push(word);
-                            wordSet.erase(word);
-                        }
+                        q.push(word);
+                        wordSet.erase(word);
                     }
-                    word[i] = cache;
                 }
+                word[i] = cache;
             }
+        }
 
         return 0;
     }
 };
+*/
