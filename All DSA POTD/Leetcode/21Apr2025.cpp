8d09699976c84e709908fa36d3de44c457a0a46e
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-04-21 21:08:22 +0530

    21:08 21 Apr

---
+++ All DSA POTD/Leetcode/21Apr2025.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int numberOfArrays(vector<int> &differences, int lower, int upper)
+    {
+        vector<long> prefix(differences.size() + 1);
+
+        for (int i = 0; i < differences.size(); ++i)
+            prefix[i + 1] += prefix[i] + differences[i];
+
+        const long mx = ranges::max(prefix);
+        const long mn = ranges::min(prefix);
+        return max(0L, (upper - lower) - (mx - mn) + 1);
+    }
+};
