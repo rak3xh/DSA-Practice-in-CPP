636112f8b3d9e27960ca9ad29331fa97712da943
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-04-21 21:07:46 +0530

    21:07 21 Apr

---
+++ All DSA POTD/GFG/21Apr2025.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    int missingNum(vector<int> &arr)
+    {
+        // code here
+        long long int n = arr.size();
+        long long int expectedSum = ((n + 1) * (n + 2)) / 2;
+        long long int actualSum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            actualSum += arr[i];
+        }
+        long long int ans = expectedSum - actualSum;
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--)
+    {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.missingNum(a) << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
