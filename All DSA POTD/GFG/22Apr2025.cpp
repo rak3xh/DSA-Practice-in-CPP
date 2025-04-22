ac4732a55fdeeb6f966c7087bf4a7be330476e43
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-04-22 19:52:17 +0530

    19:52 22 Apr

---
+++ All DSA POTD/GFG/22Apr2025.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    int findUnique(vector<int> &arr)
+    {
+        // code here
+        int n = arr.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            ans ^= arr[i];
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.findUnique(arr);
+        cout << ans << endl;
+    }
+    return 0;
+}
