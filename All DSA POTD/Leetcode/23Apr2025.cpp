51ddb9dc37b4c42f8613be20dbd1bbc18ffdea46
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-04-23 21:57:38 +0530

    21:57 23 Apr

--- All DSA POTD/Leetcode/23Apr2025.cpp
+++ All DSA POTD/Leetcode/23Apr2025.cpp
@@ -1,3 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int countLargestGroup(int n)
+    {
+        vector<int> count(9 * 4 + 1);
+        for (int i = 1; i <= n; ++i)
+            ++count[getDigitSum(i)];
+
+        int maxCount = *max_element(count.begin(), count.end());
+        return count_if(count.begin(), count.end(), [maxCount](int c)
+                        { return c == maxCount; });
+    }
+
+private:
+    int getDigitSum(int num)
+    {
+        int digitSum = 0;
+        while (num > 0)
+        {
+            digitSum += num % 10;
+            num /= 10;
+        }
+        return digitSum;
+    }
+};
+
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
