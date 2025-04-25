c761c55cadd1c1f71efde2e9e5678300e5294c44
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-04-25 20:57:38 +0530

    20:57 25 Apr

---
+++ All DSA POTD/Leetcode/25Apr2025.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    long long countInterestingSubarrays(vector<int> &nums, int modulo, int k)
+    {
+        long ans = 0;
+        int prefix = 0; // (number of nums[i] % modulo == k so far) % modulo
+        unordered_map<int, int> prefixCount{{0, 1}};
+
+        for (const int num : nums)
+        {
+            if (num % modulo == k)
+                prefix = (prefix + 1) % modulo;
+            ans += prefixCount[(prefix - k + modulo) % modulo];
+            ++prefixCount[prefix];
+        }
+
+        return ans;
+    }
+};
