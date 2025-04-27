871bad6a4a28adc0b17f2df519f3806782a68f8e
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-04-27 13:41:59 +0530

    13:41 27 Apr

---
+++ All DSA POTD/Leetcode/27Apr2025.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int countSubarrays(vector<int> &nums)
+    {
+        int ans = 0;
+
+        for (int i = 1; i + 1 < nums.size(); ++i)
+            if (nums[i] == (nums[i - 1] + nums[i + 1]) * 2)
+                ++ans;
+
+        return ans;
+    }
+};
