de5602e2fba6eb502b646e7e05268e0a6c14e4b1
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-05-02 21:37:18 +0530

    21:37 02 May

---
+++ All DSA POTD/GFG/02May2025.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+// User function template for C++
+class Solution
+{
+public:
+    int findMaximum(vector<int> &arr)
+    {
+        // code here
+        return *max_element(arr.begin(), arr.end());
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--)
+    {
+        vector<int> arr;
+        int value;
+
+        // Read the entire line of integers
+        string line;
+        // cin.ignore(); // Ignore the newline after the test case input
+        getline(cin, line);
+        stringstream ss(line);
+
+        // Parse integers and add them to the vector
+        while (ss >> value)
+        {
+            arr.push_back(value);
+        }
+
+        Solution ob;
+        auto ans = ob.findMaximum(arr);
+        cout << ans << "\n";
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
