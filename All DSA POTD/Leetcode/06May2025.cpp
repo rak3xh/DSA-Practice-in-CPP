f82c3d9d389c2cdbedb32b2ee5b3220a49b7cebc
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-05-06 20:23:04 +0530

    20:22 06 May

---
+++ All DSA POTD/Leetcode/06May2025.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> buildArray(vector<int> &nums)
+    {
+        const int n = nums.size();
+
+        for (int i = 0; i < n; ++i)
+            nums[i] += n * (nums[nums[i]] % n);
+
+        for (int i = 0; i < n; ++i)
+            nums[i] /= n;
+
+        return nums;
+    }
+};
