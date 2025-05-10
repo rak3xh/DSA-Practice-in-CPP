f723c3a1b057e7cfffd3be71b4938679cc2de922
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-05-10 14:27:09 +0530

    14:27 10 May

--- All DSA POTD/Leetcode/10May2025.cpp
+++ All DSA POTD/Leetcode/10May2025.cpp
@@ -1,4 +1,22 @@
-// C++ 20 Solution
+// C++ 14 Solution
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    public:
+    long long minSum(vector<int> &nums1, vector<int> &nums2)
+    {
+        const long sum1 = accumulate(nums1.begin(), nums1.end(), 0L);
+        const long sum2 = accumulate(nums2.begin(), nums2.end(), 0L);
+        const int zero1 = count(nums1.begin(), nums1.end(), 0);
+        const int zero2 = count(nums2.begin(), nums2.end(), 0);
+        if (zero1 == 0 && sum1 < sum2 + zero2)
+        return -1;
+        if (zero2 == 0 && sum2 < sum1 + zero1)
+        return -1;
+        return max(sum1 + zero1, sum2 + zero2);
+    }
+};
 /*
 #include <bits/stdc++.h>
 using namespace std;
