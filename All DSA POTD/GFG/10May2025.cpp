f84e877d133236c0f41e2187c9e2a66a9b61d3dd
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-05-10 14:27:49 +0530

    14:27 10 May

---
+++ All DSA POTD/GFG/10May2025.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+// User function Template for C++
+class Solution
+{
+public:
+    int longestSubarray(vector<int> &arr, int k)
+    {
+        // Code here
+        int c = 0, ans = 0;
+        unordered_map<int, int> m;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (arr[i] <= k)
+                c--;
+            else
+                c++;
+            if (c <= 0 and !m.count(c))
+                m[c] = i;
+            if (c <= 0 and m.count(c - 1))
+                ans = max(ans, i - m[c - 1]);
+            if (c > 0)
+                ans = i + 1;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        int k = stoi(input);
+
+        Solution ob;
+        cout << ob.longestSubarray(arr, k) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
