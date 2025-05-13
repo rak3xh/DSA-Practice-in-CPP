26c3d566cbd5bfd82dc63ebd873082b4b629aa91
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-05-13 19:57:34 +0530

    19:57 13 May

--- All DSA POTD/Leetcode/13May2025MatrixApproach.cpp
+++ All DSA POTD/Leetcode/13May2025MatrixApproach.cpp
@@ -1,3 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthAfterTransformations(string s, int t)
+    {
+        const vector<vector<int>> T = getTransformationMatrix();
+        const vector<vector<int>> poweredT = matrixPow(T, t);
+        vector<int> count(26, 0);
+        vector<long long> lengths(26, 0);
+
+        for (const char c : s)
+            ++count[c - 'a'];
+
+        for (int i = 0; i < 26; ++i)
+        {
+            for (int j = 0; j < 26; ++j)
+            {
+                lengths[j] += static_cast<long long>(count[i]) * poweredT[i][j];
+                lengths[j] %= kMod;
+            }
+        }
+
+        return accumulate(lengths.begin(), lengths.end(), 0LL) % kMod;
+    }
+
+private:
+    static const int kMod = 1000000007;
+
+    vector<vector<int>> getTransformationMatrix()
+    {
+        vector<vector<int>> T(26, vector<int>(26, 0));
+        T[25][0] = 1;
+        T[25][1] = 1;
+        for (int i = 0; i < 25; ++i)
+            T[i][i + 1] = 1;
+        return T;
+    }
+
+    vector<vector<int>> getIdentityMatrix(int sz)
+    {
+        vector<vector<int>> I(sz, vector<int>(sz, 0));
+        for (int i = 0; i < sz; ++i)
+            I[i][i] = 1;
+        return I;
+    }
+
+    vector<vector<int>> matrixMult(const vector<vector<int>> &A, const vector<vector<int>> &B)
+    {
+        const int sz = A.size();
+        vector<vector<int>> C(sz, vector<int>(sz, 0));
+        for (int i = 0; i < sz; ++i)
+        {
+            for (int j = 0; j < sz; ++j)
+            {
+                for (int k = 0; k < sz; ++k)
+                {
+                    C[i][j] = (C[i][j] + static_cast<long long>(A[i][k]) * B[k][j]) % kMod;
+                }
+            }
+        }
+        return C;
+    }
+
+    vector<vector<int>> matrixPow(const vector<vector<int>> &M, int n)
+    {
+        if (n == 0)
+            return getIdentityMatrix(M.size());
+        if (n % 2 == 1)
+            return matrixMult(M, matrixPow(M, n - 1));
+        return matrixPow(matrixMult(M, M), n / 2);
+    }
+};
 // Matrix Approach C++ 20
 /*
 #include <bits/stdc++.h>
