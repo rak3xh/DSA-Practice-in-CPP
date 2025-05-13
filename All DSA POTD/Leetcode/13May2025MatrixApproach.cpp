5687a36024426f295bc92f498446bdc6724fc64e
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-05-13 19:55:28 +0530

    19:55 13 May

--- All DSA POTD/Leetcode/13May2025MatrixApproach.cpp
+++ All DSA POTD/Leetcode/13May2025MatrixApproach.cpp
@@ -1,9 +1,10 @@
 // Matrix Approach C++ 20
+/*
 #include <bits/stdc++.h>
 using namespace std;
 class Solution
 {
-public:
+    public:
     int lengthAfterTransformations(string s, int t)
     {
         // T[i][j] := the number of ways to transform ('a' + i) to ('a' + j)
@@ -14,19 +15,19 @@ public:
         vector<long> lengths(26);
 
         for (const char c : s)
-            ++count[c - 'a'];
+        ++count[c - 'a'];
 
         for (int i = 0; i < 26; ++i)
-            for (int j = 0; j < 26; ++j)
-            {
-                lengths[j] += static_cast<long>(count[i]) * poweredT[i][j];
-                lengths[j] %= kMod;
-            }
+        for (int j = 0; j < 26; ++j)
+        {
+            lengths[j] += static_cast<long>(count[i]) * poweredT[i][j];
+            lengths[j] %= kMod;
+        }
 
         return accumulate(lengths.begin(), lengths.end(), 0L) % kMod;
     }
 
-private:
+    private:
     static constexpr int kMod = 1'000'000'007;
 
     vector<vector<int>> getTransformationMatrix()
@@ -37,7 +38,7 @@ private:
         T[25][1] = 1;
         // 'a' -> 'b', 'b' -> 'c', ..., 'y' -> 'z'
         for (int i = 0; i < 25; ++i)
-            T[i][i + 1] = 1;
+        T[i][i + 1] = 1;
         return T;
     }
 
@@ -45,20 +46,20 @@ private:
     {
         vector<vector<int>> I(sz, vector<int>(sz));
         for (int i = 0; i < sz; ++i)
-            I[i][i] = 1;
+        I[i][i] = 1;
         return I;
     }
 
     // Returns A * B.
     vector<vector<int>> matrixMult(const vector<vector<int>> &A,
-                                   const vector<vector<int>> &B)
+    const vector<vector<int>> &B)
     {
         const int sz = A.size();
         vector<vector<int>> C(sz, vector<int>(sz));
         for (int i = 0; i < sz; ++i)
-            for (int j = 0; j < sz; ++j)
-                for (int k = 0; k < sz; ++k)
-                    C[i][j] = (C[i][j] + static_cast<long>(A[i][k]) * B[k][j]) % kMod;
+        for (int j = 0; j < sz; ++j)
+        for (int k = 0; k < sz; ++k)
+        C[i][j] = (C[i][j] + static_cast<long>(A[i][k]) * B[k][j]) % kMod;
         return C;
     }
 
@@ -66,9 +67,10 @@ private:
     vector<vector<int>> matrixPow(const vector<vector<int>> &M, int n)
     {
         if (n == 0)
-            return getIdentityMatrix(M.size());
+        return getIdentityMatrix(M.size());
         if (n % 2 == 1)
-            return matrixMult(M, matrixPow(M, n - 1));
+        return matrixMult(M, matrixPow(M, n - 1));
         return matrixPow(matrixMult(M, M), n / 2);
     }
 };
+*/
