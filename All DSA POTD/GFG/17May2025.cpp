7a9345df3d50db32ab4a62bf4aafaeeceae72340
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-05-17 15:57:45 +0530

    15:57 17 May

---
+++ All DSA POTD/GFG/17May2025.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    vector<int> sortArray(vector<int> &arr, int A, int B, int C)
+    {
+        // code here
+        vector<int> ans;
+        for (auto it : arr)
+        {
+            ans.push_back(A * it * it + B * it + C);
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        vector<int> arr;
+        string input;
+
+        getline(cin, input);
+
+        stringstream ss(input);
+        int number;
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+
+        int a, b, c;
+        cin >> a >> b >> c;
+        cin.ignore();
+
+        Solution obj;
+        vector<int> ans = obj.sortArray(arr, a, b, c);
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << ' ';
+        cout << endl;
+
+        cout << "~" << endl;
+    }
+    return 0;
+}
