b07bd5c55a4b3e8ee663deb27c49996d09285fb9
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-05-19 22:11:10 +0530

    22:11 19 May

--- All DSA POTD/Leetcode/19May2025.cpp
+++ All DSA POTD/Leetcode/19May2025.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string triangleType(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        if (nums[0] + nums[1] <= nums[2])
+            return "none";
+        if (nums[0] == nums[1] && nums[1] == nums[2])
+            return "equilateral";
+        if (nums[0] == nums[1] || nums[1] == nums[2])
+            return "isosceles";
+        return "scalene";
+    }
+};
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
