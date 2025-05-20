f90ce2aab4a499347eaae72cd121fd46759236d1
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-05-20 21:52:09 +0530

    21:52 20 May

---
+++ All DSA POTD/Leetcode/20May2025.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool isZeroArray(vector<int> &nums, vector<vector<int>> &queries)
+    {
+        vector<int> line(nums.size() + 1);
+        int decrement = 0;
+
+        for (const vector<int> &query : queries)
+        {
+            const int l = query[0];
+            const int r = query[1];
+            ++line[l];
+            --line[r + 1];
+        }
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            decrement += line[i];
+            if (decrement < nums[i])
+                return false;
+        }
+
+        return true;
+    }
+};
