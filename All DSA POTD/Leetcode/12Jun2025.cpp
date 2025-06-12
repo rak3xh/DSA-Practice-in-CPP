cd17ffdb8239796ef06c6af97b41e1bf9e619a60
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-06-12 19:21:53 +0530

    19:21 12 Jun

---
+++ All DSA POTD/Leetcode/12Jun2025.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxAdjacentDistance(vector<int> &nums)
+    {
+        int ans = abs(nums.front() - nums.back());
+
+        for (int i = 0; i + 1 < nums.size(); ++i)
+            ans = max(ans, abs(nums[i] - nums[i + 1]));
+
+        return ans;
+    }
+};
