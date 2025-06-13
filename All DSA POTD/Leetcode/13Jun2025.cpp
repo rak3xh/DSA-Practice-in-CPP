437eb682b6ac887418643a5b691031c785f0d107
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-06-13 19:14:43 +0530

    19:14 13 Jun

--- All DSA POTD/Leetcode/13Jun2025.cpp
+++ All DSA POTD/Leetcode/13Jun2025.cpp
@@ -1,3 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minimizeMax(vector<int> &nums, int p)
+    {
+        sort(nums.begin(), nums.end());
+
+        int l = 0;
+        int r = nums.back() - nums.front();
+
+        while (l < r)
+        {
+            const int m = (l + r) / 2;
+            if (numPairs(nums, m) >= p)
+                r = m;
+            else
+                l = m + 1;
+        }
+
+        return l;
+    }
+
+private:
+    // Returns the number of pairs that can be obtained if the difference between
+    // each pair <= `maxDiff`.
+    int numPairs(const vector<int> &nums, int maxDiff)
+    {
+        int pairs = 0;
+        for (int i = 1; i < nums.size(); ++i)
+            // Greedily pair nums[i] with nums[i - 1].
+            if (nums[i] - nums[i - 1] <= maxDiff)
+            {
+                ++pairs;
+                ++i;
+            }
+        return pairs;
+    }
+};
 // C++ 20 Solution
 /*
 #include <bits/stdc++.h>
