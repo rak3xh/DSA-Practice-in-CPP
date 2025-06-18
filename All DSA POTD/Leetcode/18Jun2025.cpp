67d1a29e1a7537f165c20b258b691b673ea5dc10
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-06-18 21:23:14 +0530

    21:23 18 Jun

--- All DSA POTD/Leetcode/18Jun2025.cpp
+++ All DSA POTD/Leetcode/18Jun2025.cpp
@@ -7,7 +7,7 @@ public:
     {
         vector<vector<int>> ans;
 
-        ranges::sort(nums);
+        sort(nums.begin(), nums.end());
 
         for (int i = 2; i < nums.size(); i += 3)
         {
@@ -19,3 +19,24 @@ public:
         return ans;
     }
 };
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Solution
+// {
+// public:
+//     vector<vector<int>> divideArray(vector<int> &nums, int k)
+//     {
+//         vector<vector<int>> ans;
+
+//         ranges::sort(nums);
+
+//         for (int i = 2; i < nums.size(); i += 3)
+//         {
+//             if (nums[i] - nums[i - 2] > k)
+//                 return {};
+//             ans.push_back({nums[i - 2], nums[i - 1], nums[i]});
+//         }
+
+//         return ans;
+//     }
+// };
