6ad67e762e24600222b04a1c5e25bd810f909424
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-06-20 21:50:12 +0530

    21:50 20 Jun

---
+++ All DSA POTD/GFG/20Jun2025.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool validgroup(vector<int> &arr, int k)
+    {
+        // code here
+        if (arr.size() % k != 0)
+            return false;
+        sort(arr.begin(), arr.end());
+
+        int cnt = 1;
+
+        for (int i = 1; i < arr.size(); i++)
+        {
+            if (arr[i] - arr[i - 1] == 1)
+            {
+                cnt++;
+            }
+            else if (arr[i] == arr[i - 1])
+            {
+                continue; // allow duplicates
+            }
+            else
+            {
+                if (cnt % k != 0)
+                    return false;
+                cnt = 1;
+            }
+        }
+
+        return cnt % k == 0;
+    }
+};
