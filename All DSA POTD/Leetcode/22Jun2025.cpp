c70642be2f7789970a5be9653b716c6c93a076f6
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-06-22 20:35:04 +0530

    20:34 22 Jun

---
+++ All DSA POTD/Leetcode/22Jun2025.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<string> divideString(string s, int k, char fill)
+    {
+        vector<string> ans;
+
+        for (int i = 0; i < s.length(); i += k)
+            ans.push_back(i + k > s.length()
+                              ? s.substr(i) + string(i + k - s.length(), fill)
+                              : s.substr(i, k));
+
+        return ans;
+    }
+};
