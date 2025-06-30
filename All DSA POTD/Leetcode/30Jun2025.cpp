eb9fa24164118428913db48f375b3dbdd80de15b
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-06-30 21:04:02 +0530

    21:03 30 Jun

--- All DSA POTD/Leetcode/30Jun2025.cpp
+++ All DSA POTD/Leetcode/30Jun2025.cpp
@@ -1,3 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findLHS(vector<int> &nums)
+    {
+        int ans = 0;
+        unordered_map<int, int> count;
+
+        for (const int num : nums)
+            ++count[num];
+
+        for (auto it = count.begin(); it != count.end(); ++it)
+        {
+            int num = it->first;
+            int freq = it->second;
+            if (count.find(num + 1) != count.end())
+                ans = max(ans, freq + count[num + 1]);
+        }
+
+        return ans;
+    }
+};
+
 // C++ 20 Solution
 //  #include <bits/stdc++.h>
 //  using namespace std;
