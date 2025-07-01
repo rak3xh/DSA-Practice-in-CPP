281ff9bd2c7b9d3d25e3e93a8a097ed036d3f834
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-07-01 22:01:58 +0530

    22:01 01 Jul

---
+++ All DSA POTD/Leetcode/01Jul2025.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int possibleStringCount(string word)
+    {
+        int ans = 1;
+        for (int i = 1; i < word.length(); ++i)
+            if (word[i] == word[i - 1])
+                ++ans;
+        return ans;
+    }
+};
