27682757287d350fa173c834b3a1e980f7b16dad
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-07-03 22:57:56 +0530

    22:57 03 Jul

---
+++ All DSA POTD/Leetcode/03Jul2025.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    char kthCharacter(int k)
+    {
+        vector<int> word;
+        word.push_back(0);
+        while (word.size() < k)
+        {
+            int m = word.size();
+            for (int i = 0; i < m; ++i)
+            {
+                word.push_back((word[i] + 1) % 26);
+            }
+        }
+        return 'a' + word[k - 1];
+    }
+};
