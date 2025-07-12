7e479e99144874a14b681f7a96edc8f3895e7776
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-07-12 14:47:21 +0530

    14:47 12 Jul

--- All DSA POTD/Leetcode/12Jul2025.cpp
+++ All DSA POTD/Leetcode/12Jul2025.cpp
@@ -1,3 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> earliestAndLatest(int n, int firstPlayer, int secondPlayer)
+    {
+        using P = pair<int, int>;
+        vector<vector<vector<P>>> mem(n + 1, vector<vector<P>>(n + 1, vector<P>(n + 1)));
+
+        pair<int, int> res = solve(firstPlayer, n - secondPlayer + 1, n, mem);
+        int a = res.first;
+        int b = res.second;
+
+        return {a, b};
+    }
+
+private:
+    // Returns the (earliest, latest) pair.
+    pair<int, int> solve(int l, int r, int k,
+                         vector<vector<vector<pair<int, int>>>> &mem)
+    {
+        if (l == r)
+            return {1, 1};
+        if (l > r)
+            swap(l, r);
+        if (mem[l][r][k] != pair<int, int>{0, 0})
+            return mem[l][r][k];
+
+        int a = INT_MAX;
+        int b = INT_MIN;
+
+        for (int i = 1; i <= l; ++i)
+        {
+            for (int j = l - i + 1; j <= r - i; ++j)
+            {
+                if (i + j > (k + 1) / 2 || i + j < l + r - k / 2)
+                    continue;
+                pair<int, int> res = solve(i, j, (k + 1) / 2, mem);
+                a = min(a, res.first + 1);
+                b = max(b, res.second + 1);
+            }
+        }
+
+        return mem[l][r][k] = {a, b};
+    }
+};
+
 // C++ 20 Solution
 //  #include <bits/stdc++.h>
 //  using namespace std;
