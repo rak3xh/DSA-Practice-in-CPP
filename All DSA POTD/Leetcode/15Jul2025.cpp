e9240964ce6f4c65af33a07106e7a618dae0d018
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-07-15 21:16:51 +0530

    21:16 15 Jul

--- All DSA POTD/Leetcode/15Jul2025.cpp
+++ All DSA POTD/Leetcode/15Jul2025.cpp
@@ -1,3 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValid(string word)
+    {
+        if (word.length() < 3)
+            return false;
+
+        bool hasVowel = false;
+        bool hasConsonant = false;
+
+        for (char c : word)
+        {
+            if (!isalnum(c))
+                return false;
+
+            if (isVowel(c))
+                hasVowel = true;
+            else if (isConsonant(c))
+                hasConsonant = true;
+        }
+
+        return hasVowel && hasConsonant;
+    }
+
+private:
+    bool isVowel(char c)
+    {
+        char lower = tolower(c);
+        return lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u';
+    }
+
+    bool isConsonant(char c)
+    {
+        return isalpha(c) && !isVowel(c);
+    }
+};
+
 // C++ 20 Solution
 //  #include <bits/stdc++.h>
 //  using namespace std;
