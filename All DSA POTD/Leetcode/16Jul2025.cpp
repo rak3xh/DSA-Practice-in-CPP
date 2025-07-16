c1c86abcdc8b945098f9c3a5aa0592b4df9ca90f
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-07-16 21:42:04 +0530

    21:41 16 Jul

--- All DSA POTD/Leetcode/16Jul2025.cpp
+++ All DSA POTD/Leetcode/16Jul2025.cpp
@@ -1,21 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
-class Solution
-{
-public:
-    int maximumLength(vector<int> &nums)
-    {
-        vector<vector<int>> dp(2, vector<int>(2));
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Solution
+// {
+// public:
+//     int maximumLength(vector<int> &nums)
+//     {
+//         vector<vector<int>> dp(2, vector<int>(2));
 
-        // Extend the pattern xyxyxy...xy.
-        for (const int x : nums)
-            for (int y = 0; y < 2; ++y)
-                dp[x % 2][y] = dp[y][x % 2] + 1;
+//         // Extend the pattern xyxyxy...xy.
+//         for (const int x : nums)
+//             for (int y = 0; y < 2; ++y)
+//                 dp[x % 2][y] = dp[y][x % 2] + 1;
 
-        return accumulate(dp.begin(), dp.end(), 0,
-                          [](int acc, const vector<int> &row)
-                          {
-                              return max(acc, ranges::max(row));
-                          });
-    }
-};
+//         return accumulate(dp.begin(), dp.end(), 0,
+//                           [](int acc, const vector<int> &row)
+//                           {
+//                               return max(acc, ranges::max(row));
+//                           });
+//     }
+// };
