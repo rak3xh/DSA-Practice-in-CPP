4f2c0154218da5253813ee34087e9b45afdee67d
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-07-19 12:25:04 +0530

    12:24 19 Jul

---
+++ All DSA POTD/GFG/19Jul2025.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int vowelCount(string &s)
+    {
+        // code here
+        unordered_map<int, int> mp;
+        for (auto it : s)
+        {
+            if (it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u')
+            {
+                mp[it]++;
+            }
+        }
+        if (mp.size() == 0)
+            return 0;
+        int ans = 1;
+        for (auto it : mp)
+        {
+            ans *= it.second;
+        }
+        int sz = mp.size();
+        while (sz > 0)
+        {
+            ans *= sz;
+            sz--;
+        }
+        return ans;
+    }
+};
