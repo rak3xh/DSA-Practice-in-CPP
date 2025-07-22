e00ae17a99e1e0894ded0a9a35111656945a8175
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-07-22 19:42:36 +0530

    19:42 22 Jul

---
+++ All DSA POTD/Leetcode/22Jul2025.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maximumUniqueSubarray(vector<int> &nums)
+    {
+        int ans = 0;
+        int score = 0;
+        unordered_set<int> seen;
+
+        for (int l = 0, r = 0; r < nums.size(); ++r)
+        {
+            while (!seen.insert(nums[r]).second)
+            {
+                score -= nums[l];
+                seen.erase(nums[l++]);
+            }
+            score += nums[r];
+            ans = max(ans, score);
+        }
+
+        return ans;
+    }
+};
