5a4eee2c48026f6dda50112f6708b158bf280ace
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-07-22 19:41:43 +0530

    19:41 22 Jul

---
+++ All DSA POTD/Leetcode/21Jul2025.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string makeFancyString(string s)
+    {
+        string ans;
+        for (const char c : s)
+            if (ans.length() < 2 || ans.back() != c || ans[ans.size() - 2] != c)
+                ans.push_back(c);
+        return ans;
+    }
+};
