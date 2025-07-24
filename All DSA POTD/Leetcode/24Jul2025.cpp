8ede8da616fd569530300325bb770663256c7739
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-07-24 20:27:13 +0530

    20:27 24 Jul

--- All DSA POTD/Leetcode/24Jul2025.cpp
+++ All DSA POTD/Leetcode/24Jul2025.cpp
@@ -1,3 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minimumScore(vector<int> &nums, vector<vector<int>> &edges)
+    {
+        const int n = nums.size();
+
+        // Replace std::reduce with manual XOR accumulation
+        int xors = 0;
+        for (int num : nums)
+            xors ^= num;
+
+        vector<int> subXors = nums;
+        vector<vector<int>> tree(n);
+        vector<unordered_set<int>> children(n);
+
+        for (int i = 0; i < n; ++i)
+            children[i].insert(i);
+
+        for (const vector<int> &edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+            tree[u].push_back(v);
+            tree[v].push_back(u);
+        }
+
+        dfs(tree, 0, -1, subXors, children);
+
+        int ans = INT_MAX;
+
+        for (int i = 0; i < edges.size(); ++i)
+        {
+            int a = edges[i][0];
+            int b = edges[i][1];
+            if (children[a].count(b))
+                swap(a, b);
+            for (int j = 0; j < i; ++j)
+            {
+                int c = edges[j][0];
+                int d = edges[j][1];
+                if (children[c].count(d))
+                    swap(c, d);
+
+                vector<int> cands;
+
+                if (a != c && children[a].count(c))
+                    cands = {subXors[c], subXors[a] ^ subXors[c], xors ^ subXors[a]};
+                else if (a != c && children[c].count(a))
+                    cands = {subXors[a], subXors[c] ^ subXors[a], xors ^ subXors[c]};
+                else
+                    cands = {subXors[a], subXors[c], xors ^ subXors[a] ^ subXors[c]};
+
+                int maxVal = *max_element(cands.begin(), cands.end());
+                int minVal = *min_element(cands.begin(), cands.end());
+
+                ans = min(ans, maxVal - minVal);
+            }
+        }
+
+        return ans;
+    }
+
+private:
+    pair<int, unordered_set<int>> dfs(const vector<vector<int>> &tree, int u,
+                                      int prev, vector<int> &subXors,
+                                      vector<unordered_set<int>> &children)
+    {
+        for (int v : tree[u])
+        {
+            if (v == prev)
+                continue;
+
+            pair<int, unordered_set<int>> result = dfs(tree, v, u, subXors, children);
+            int vXor = result.first;
+            unordered_set<int> &vChildren = result.second;
+
+            subXors[u] ^= vXor;
+            children[u].insert(vChildren.begin(), vChildren.end());
+        }
+        return {subXors[u], children[u]};
+    }
+};
+
 // C++ 20 Solution
 //  #include <bits/stdc++.h>
 //  using namespace std;
