2ebe34deb7b4b0817d9bd95f42cb6daa328cdc2b
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-07-25 22:40:41 +0530

    22:40 25 Jul

--- All DSA POTD/Leetcode/25Jul2025.cpp
+++ All DSA POTD/Leetcode/25Jul2025.cpp
@@ -1,3 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxSum(vector<int> &nums)
+    {
+        const int mx = *max_element(nums.begin(), nums.end());
+        if (mx <= 0)
+            return mx;
+        unordered_set<int> numsSet(nums.begin(), nums.end());
+        return accumulate(numsSet.begin(), numsSet.end(), 0,
+                          [](int acc, int num)
+                          { return acc + max(0, num); });
+    }
+};
 // C++ 20 Solution
 //  #include <bits/stdc++.h>
 //  using namespace std;
