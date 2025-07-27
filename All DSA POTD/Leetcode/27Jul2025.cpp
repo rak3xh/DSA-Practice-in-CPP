fe0774d51e3442cdb6b85d2d5cf8984bc48c67e5
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-07-27 22:36:37 +0530

    22:36 27 Jul

---
+++ All DSA POTD/Leetcode/27Jul2025.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int countHillValley(vector<int> &nums)
+    {
+        int ans = 0;
+        int left = nums[0];
+
+        for (int i = 1; i + 1 < nums.size(); ++i)
+            if (left < nums[i] && nums[i] > nums[i + 1] || // the hill
+                left > nums[i] && nums[i] < nums[i + 1])
+            { // the valley
+                ++ans;
+                left = nums[i];
+            }
+
+        return ans;
+    }
+};
