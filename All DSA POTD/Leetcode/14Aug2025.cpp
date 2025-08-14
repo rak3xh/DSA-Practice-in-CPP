207887dd54a28e94b468d896043636dceae9ec27
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-08-14 23:20:45 +0530

    23:20 14 Aug

---
+++ All DSA POTD/Leetcode/14Aug2025.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string largestGoodInteger(string num)
+    {
+        string ans;
+
+        for (int i = 2; i < num.length(); ++i)
+            if (num[i - 2] == num[i - 1] && num[i - 1] == num[i])
+                ans = max(ans, num.substr(i - 2, 3));
+
+        return ans;
+    }
+};
