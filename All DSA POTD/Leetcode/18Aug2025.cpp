9e00a3b81a5f330902e1cd8389ca5f1d8a2ea023
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-08-18 23:10:17 +0530

    23:10 18 Aug

---
+++ All DSA POTD/Leetcode/18Aug2025.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool judgePoint24(vector<int> &cards)
+    {
+        vector<double> doubleNums;
+
+        for (const int num : cards)
+            doubleNums.push_back(num);
+
+        return dfs(doubleNums);
+    }
+
+private:
+    bool dfs(vector<double> &nums)
+    {
+        if (nums.size() == 1)
+            return abs(nums[0] - 24) < 0.001;
+
+        for (int i = 0; i < nums.size(); ++i)
+            for (int j = 0; j < i; ++j)
+            {
+                for (const double num : generate(nums[i], nums[j]))
+                {
+                    vector<double> nextRound{num};
+                    for (int k = 0; k < nums.size(); ++k)
+                    {
+                        if (k == i || k == j) // It is used in `generate()`.
+                            continue;
+                        nextRound.push_back(nums[k]);
+                    }
+                    if (dfs(nextRound))
+                        return true;
+                }
+            }
+
+        return false;
+    }
+
+    vector<double> generate(double a, double b)
+    {
+        return {a * b, a / b, b / a, a + b, a - b, b - a};
+    }
+};
