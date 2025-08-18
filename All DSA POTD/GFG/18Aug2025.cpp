10a1df23ba7aaf38096b74e9fe5ac5a579f94211
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-08-18 23:09:41 +0530

    23:09 18 Aug

---
+++ All DSA POTD/GFG/18Aug2025.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int hIndex(vector<int> &citations)
+    {
+        // code here
+        int n = citations.size();
+        sort(citations.begin(), citations.end()); // sort ascending
+        int h = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int papersWithAtLeast = n - i; // number of papers from i to n-1
+            h = max(h, min(citations[i], papersWithAtLeast));
+        }
+
+        return h;
+    }
+};
