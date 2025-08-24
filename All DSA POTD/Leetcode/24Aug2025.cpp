8e798179a2e82633ce3ae0a95909d995617f5e28
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-08-24 14:36:07 +0530

    14:36 24 Aug

---
+++ All DSA POTD/Leetcode/24Aug2025.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int longestSubarray(vector<int> &nums)
+    {
+        int ans = 0;
+        int zeros = 0;
+
+        for (int l = 0, r = 0; r < nums.size(); ++r)
+        {
+            if (nums[r] == 0)
+                ++zeros;
+            while (zeros == 2)
+                if (nums[l++] == 0)
+                    --zeros;
+            ans = max(ans, r - l);
+        }
+
+        return ans;
+    }
+};
