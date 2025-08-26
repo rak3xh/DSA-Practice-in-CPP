cdc49c3aeb155161b548404bd664b23d70550d1b
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-08-26 19:38:42 +0530

    19:38 26 Aug

--- All DSA POTD/Leetcode/26Aug2025.cpp
+++ All DSA POTD/Leetcode/26Aug2025.cpp
@@ -1,3 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int areaOfMaxDiagonal(vector<vector<int>> &dimensions)
+    {
+        auto it = max_element(dimensions.begin(), dimensions.end(),
+                              [](const vector<int> &a, const vector<int> &b)
+                              {
+                                  int diagA = a[0] * a[0] + a[1] * a[1];
+                                  int diagB = b[0] * b[0] + b[1] * b[1];
+                                  if (diagA == diagB)
+                                      return a[0] * a[1] < b[0] * b[1];
+                                  return diagA < diagB;
+                              });
+
+        return (*it)[0] * (*it)[1];
+    }
+};
+
 // C++ 20 Solution
 //  #include <bits/stdc++.h>
 //  using namespace std;
