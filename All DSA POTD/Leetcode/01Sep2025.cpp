7a024a06667ea4adf0624fce7d8dd6e22c022ab8
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-01 20:22:30 +0530

    20:22 01 Sep

--- All DSA POTD/Leetcode/01Sep2025.cpp
+++ All DSA POTD/Leetcode/01Sep2025.cpp
@@ -1,43 +1,43 @@
-#include <bits/stdc++.h>
-using namespace std;
-class Solution
-{
-public:
-    double maxAverageRatio(vector<vector<int>> &classes, int extraStudents)
-    {
-        priority_queue<tuple<double, int, int>> maxHeap;
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Solution
+// {
+// public:
+//     double maxAverageRatio(vector<vector<int>> &classes, int extraStudents)
+//     {
+//         priority_queue<tuple<double, int, int>> maxHeap;
 
-        for (const vector<int> &c : classes)
-        {
-            const int pass = c[0];
-            const int total = c[1];
-            maxHeap.emplace(extraPassRatio(pass, total), pass, total);
-        }
+//         for (const vector<int> &c : classes)
+//         {
+//             const int pass = c[0];
+//             const int total = c[1];
+//             maxHeap.emplace(extraPassRatio(pass, total), pass, total);
+//         }
 
-        for (int i = 0; i < extraStudents; ++i)
-        {
-            const auto [_, pass, total] = maxHeap.top();
-            maxHeap.pop();
-            maxHeap.emplace(extraPassRatio(pass + 1, total + 1), pass + 1, total + 1);
-        }
+//         for (int i = 0; i < extraStudents; ++i)
+//         {
+//             const auto [_, pass, total] = maxHeap.top();
+//             maxHeap.pop();
+//             maxHeap.emplace(extraPassRatio(pass + 1, total + 1), pass + 1, total + 1);
+//         }
 
-        double ratioSum = 0;
+//         double ratioSum = 0;
 
-        while (!maxHeap.empty())
-        {
-            const auto [_, pass, total] = maxHeap.top();
-            maxHeap.pop();
-            ratioSum += pass / static_cast<double>(total);
-        }
+//         while (!maxHeap.empty())
+//         {
+//             const auto [_, pass, total] = maxHeap.top();
+//             maxHeap.pop();
+//             ratioSum += pass / static_cast<double>(total);
+//         }
 
-        return ratioSum / classes.size();
-    }
+//         return ratioSum / classes.size();
+//     }
 
-private:
-    // Returns the extra pass ratio if a brilliant student joins.
-    double extraPassRatio(int pass, int total)
-    {
-        return (pass + 1) / static_cast<double>(total + 1) -
-               pass / static_cast<double>(total);
-    }
-};
+// private:
+//     // Returns the extra pass ratio if a brilliant student joins.
+//     double extraPassRatio(int pass, int total)
+//     {
+//         return (pass + 1) / static_cast<double>(total + 1) -
+//                pass / static_cast<double>(total);
+//     }
+// };
