f852d33615d246e2ae3d835d4d2b239a76f90b6c
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-03 21:54:10 +0530

    21:54 03 Sep

--- All DSA POTD/Leetcode/03Sep2025.cpp
+++ All DSA POTD/Leetcode/03Sep2025.cpp
@@ -1,4 +1,39 @@
 // C++ 14 Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int numberOfPairs(vector<vector<int>> &points)
+    {
+        int ans = 0;
+
+        // Sort by x ascending, and if x is same then y descending
+        sort(points.begin(), points.end(),
+             [](const vector<int> &a, const vector<int> &b)
+             {
+                 if (a[0] == b[0])
+                     return a[1] > b[1]; // y descending
+                 return a[0] < b[0];     // x ascending
+             });
+
+        for (int i = 0; i < (int)points.size(); ++i)
+        {
+            int maxY = INT_MIN;
+            for (int j = i + 1; j < (int)points.size(); ++j)
+            {
+                if (points[i][1] >= points[j][1] && points[j][1] > maxY)
+                {
+                    ++ans;
+                    maxY = points[j][1];
+                }
+            }
+        }
+
+        return ans;
+    }
+};
 
 //  C++ 20 Solution
 //   #include <bits/stdc++.h>
