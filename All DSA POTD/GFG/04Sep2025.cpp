773a591f26f6dcdc6ebdf8527b4ed6a54cf342ed
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-04 22:40:13 +0530

    22:40 04 Sep

---
+++ All DSA POTD/GFG/04Sep2025.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    Node *reverseKGroup(Node *head, int k)
+    {
+        // code here
+        if (k < 2)
+            return head;
+        Node *curr = head;
+        Node *prev = nullptr;
+        Node *left = nullptr;
+        Node *leftO = nullptr;
+        Node *newHead = nullptr;
+        int n = 0;
+        while (curr != nullptr)
+        {
+            n++;
+            if (n == k)
+                newHead = curr;
+            if (n % k == 1)
+            {
+                leftO = left;
+                left = curr;
+                Node *next = curr->next;
+                curr->next = nullptr;
+                prev = curr;
+                curr = next;
+            }
+            else
+            {
+                Node *next = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = next;
+            }
+            if (n % k == 0 && leftO)
+            {
+                leftO->next = prev;
+                leftO = nullptr;
+            }
+        }
+        if (leftO)
+            leftO->next = prev;
+        return newHead;
+    }
+};
