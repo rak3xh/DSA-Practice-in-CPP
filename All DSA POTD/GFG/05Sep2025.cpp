9cd5cb9319184ac158a3a78b3de67c9f926c8563
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-05 23:37:11 +0530

    23:37 05 Sep

---
+++ All DSA POTD/GFG/05Sep2025.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Node is defined as
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = nullptr;
+    }
+};
+
+class Solution
+{
+public:
+    Node *segregate(Node *head)
+    {
+        // code here
+        if (head == NULL)
+            return head;
+        Node *curr = head;
+        int cntZero = 0, cntOne = 0, cntTwo = 0;
+
+        while (curr != NULL)
+        {
+            if (curr->data == 0)
+                cntZero++;
+            if (curr->data == 1)
+                cntOne++;
+            curr = curr->next;
+        }
+
+        curr = head;
+        while (curr)
+        {
+            if (cntZero > 0)
+            {
+                curr->data = 0;
+                cntZero--;
+            }
+            else if (cntOne > 0)
+            {
+                curr->data = 1;
+                cntOne--;
+            }
+            else
+            {
+                curr->data = 2;
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+};
