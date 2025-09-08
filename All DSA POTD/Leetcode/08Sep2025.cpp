8e9a509bf1880e615f672feb174e8586ae4cbe10
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-08 21:59:31 +0530

    21:59 08 Sep

---
+++ All DSA POTD/Leetcode/08Sep2025.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        for (int A = 1; A < n; ++A) {
+      int B = n - A;
+      if (to_string(A).find('0') == string::npos &&
+          to_string(B).find('0') == string::npos)
+        return {A, B};
+    }
+
+    throw; 
+    }
+};
