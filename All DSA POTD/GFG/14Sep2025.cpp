5da81cfe51ba9b58acbefca660e5a428ed7f90c8
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-14 22:23:06 +0530

    22:23 14 Sep

---
+++ All DSA POTD/GFG/14Sep2025.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int startStation(vector<int> &gas, vector<int> &cost)
+    {
+        //  code here
+        int n = gas.size();
+        int totalGas = 0, totalCost = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+
+        // If total gas is less than total cost, it's impossible
+        if (totalGas < totalCost)
+            return -1;
+
+        int start = 0, tank = 0;
+        for (int i = 0; i < n; i++)
+        {
+            tank += gas[i] - cost[i];
+
+            // If tank goes negative, reset starting point
+            if (tank < 0)
+            {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return start;
+    }
+};
