a15f90d3c52ad4adab47996cb3f67f06bb7a9785
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-16 22:32:58 +0530

    22:32 16 Sep

---
+++ All DSA POTD/Leetcode/16Sep2025.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    // gcd implementation
+    int gcd(int a, int b)
+    {
+        while (b != 0)
+        {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+
+    // lcm implementation
+    long long lcm(long long a, long long b)
+    {
+        return (a / gcd(a, b)) * b;
+    }
+
+    vector<int> replaceNonCoprimes(vector<int> &nums)
+    {
+        vector<int> ans;
+
+        for (int num : nums)
+        {
+            while (!ans.empty() && gcd(ans.back(), num) > 1)
+            {
+                num = (int)lcm(ans.back(), num);
+                ans.pop_back();
+            }
+            ans.push_back(num);
+        }
+
+        return ans;
+    }
+};
