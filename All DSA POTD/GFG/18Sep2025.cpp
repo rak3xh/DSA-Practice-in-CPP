7bed9bf4f7e064029d73e9901fd1f94aecfc1e40
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-18 21:27:07 +0530

    21:27 18 Sep

---
+++ All DSA POTD/GFG/18Sep2025.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> nextGreater(vector<int> &arr)
+    {
+        // code here
+        int n = arr.size();
+        vector<int> result(n, -1);
+        stack<int> st; // stores indices
+
+        // Traverse the array twice to simulate circular behavior
+        for (int i = 0; i < 2 * n; i++)
+        {
+            int num = arr[i % n];
+
+            // Check if this num is the NGE for elements in stack
+            while (!st.empty() && arr[st.top()] < num)
+            {
+                result[st.top()] = num;
+                st.pop();
+            }
+
+            // Only push index during first pass
+            if (i < n)
+            {
+                st.push(i);
+            }
+        }
+        return result;
+    }
+};
