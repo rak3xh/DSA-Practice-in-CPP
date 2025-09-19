4c035965c1926f91df99604f5575396d437393fe
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-19 23:27:26 +0530

    23:27 19 Sep

---
+++ All DSA POTD/Leetcode/19Sep2025.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Spreadsheet
+{
+public:
+    Spreadsheet(int rows) {}
+
+    void setCell(string cell, int value)
+    {
+        spreadsheet[cell] = value;
+    }
+
+    void resetCell(string cell)
+    {
+        spreadsheet[cell] = 0;
+    }
+
+    int getValue(string formula)
+    {
+        const int i = formula.find('+');
+        return getToken(formula.substr(1, i - 1)) + getToken(formula.substr(i + 1));
+    }
+
+private:
+    unordered_map<string, int> spreadsheet;
+
+    int getToken(const string &token)
+    {
+        return isdigit(token[0])
+                   ? stoi(token)
+                   : (spreadsheet.contains(token) ? spreadsheet[token] : 0);
+    }
+};
