52df814a856c8de0ae3466e417fb68eb4e4fe0ae
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-22 22:38:30 +0530

    22:38 22 Sep

---
+++ All DSA POTD/Leetcode/22Sep2025.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxFrequencyElements(vector<int> &nums)
+    {
+        constexpr int kMax = 100;
+        vector<int> count(kMax + 1);
+
+        for (const int num : nums)
+            ++count[num];
+
+        const int maxFreq = ranges::max(count);
+        return ranges::count(count, maxFreq) * maxFreq;
+    }
+};
