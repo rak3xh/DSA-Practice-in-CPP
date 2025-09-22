e7f518bc2c17a46f6c7bce02374f753726e8b751
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-22 22:39:14 +0530

    22:39 22 Sep

--- All DSA POTD/Leetcode/22Sep2025.cpp
+++ All DSA POTD/Leetcode/22Sep2025.cpp
@@ -1,17 +1,46 @@
+// C++ 14 Solution
 #include <bits/stdc++.h>
 using namespace std;
+
 class Solution
 {
 public:
     int maxFrequencyElements(vector<int> &nums)
     {
-        constexpr int kMax = 100;
-        vector<int> count(kMax + 1);
+        const int kMax = 100;
+        vector<int> count(kMax + 1, 0);
 
-        for (const int num : nums)
+        for (int num : nums)
             ++count[num];
 
-        const int maxFreq = ranges::max(count);
-        return ranges::count(count, maxFreq) * maxFreq;
+        // find the maximum frequency
+        int maxFreq = *max_element(count.begin(), count.end());
+
+        // count how many elements have that frequency
+        int freqCount = 0;
+        for (int c : count)
+            if (c == maxFreq)
+                ++freqCount;
+
+        return freqCount * maxFreq;
     }
 };
+
+// C++ 20 Solution
+//  #include <bits/stdc++.h>
+//  using namespace std;
+//  class Solution
+//  {
+//  public:
+//      int maxFrequencyElements(vector<int> &nums)
+//      {
+//          constexpr int kMax = 100;
+//          vector<int> count(kMax + 1);
+
+//         for (const int num : nums)
+//             ++count[num];
+
+//         const int maxFreq = ranges::max(count);
+//         return ranges::count(count, maxFreq) * maxFreq;
+//     }
+// };
