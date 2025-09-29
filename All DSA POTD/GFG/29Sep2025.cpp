b86b682e58ae3e993713429629e2151ca0252ebe
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-29 22:47:37 +0530

    22:47 29 Sep

--- All DSA POTD/GFG/29Sep2025.cpp
+++ All DSA POTD/GFG/29Sep2025.cpp
@@ -1,29 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-// class Solution
-// {
-// public:
-//     void findSubArr(vector<int> &arr, int a, int b, vector<vector<int>> &subArrays)
-//     {
-//         int n = arr.size();
-//         for (int i = 0; i < n; i++)
-//         {
-//             vector<int> temp;
-//             for (int j = i; j < n; j++)
-//             {
-//                 temp.push_back(arr[j]);
-//                 if (temp.size() >= a && temp.size() <= b)
-//                 {
-//                     subArrays.push_back(temp);
-//                 }
-//             }
-//         }
-//     }
-//     int maxSubarrSum(vector<int> &arr, int a, int b)
-//     {
-//         // code here
-//         vector<vector<int>> subArrays;
-//         findSubArr(arr, a, b, subArrays);
+
+// Not Optimal solution
+//  class Solution
+//  {
+//  public:
+//      void findSubArr(vector<int> &arr, int a, int b, vector<vector<int>> &subArrays)
+//      {
+//          int n = arr.size();
+//          for (int i = 0; i < n; i++)
+//          {
+//              vector<int> temp;
+//              for (int j = i; j < n; j++)
+//              {
+//                  temp.push_back(arr[j]);
+//                  if (temp.size() >= a && temp.size() <= b)
+//                  {
+//                      subArrays.push_back(temp);
+//                  }
+//              }
+//          }
+//      }
+//      int maxSubarrSum(vector<int> &arr, int a, int b)
+//      {
+//          // code here
+//          vector<vector<int>> subArrays;
+//          findSubArr(arr, a, b, subArrays);
 
 //         vector<int> arrSum;
 //         for (auto i : subArrays)
