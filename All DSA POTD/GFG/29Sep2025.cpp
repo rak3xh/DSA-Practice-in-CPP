c3770b740b380dd6dc58bd9fba348dc43e3e43a8
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-29 22:47:53 +0530

    22:47 29 Sep

--- All DSA POTD/GFG/29Sep2025.cpp
+++ All DSA POTD/GFG/29Sep2025.cpp
@@ -1,5 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Solution
+{
+public:
+    int maxSubarrSum(vector<int> &arr, int a, int b)
+    {
+        // code here
+        int n = arr.size();
+        int ans = INT_MIN;
+        deque<int> q;
+
+        vector<int> pre(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+            pre[i] = pre[i - 1] + arr[i - 1];
+
+        for (int i = a; i <= n; i++)
+        {
+            while (!q.empty() && q.front() + b < i)
+                q.pop_front();
+            while (!q.empty() && pre[q.back()] > pre[i - a])
+                q.pop_back();
+            q.push_back(i - a);
+
+            ans = max(ans, pre[i] - pre[q.front()]);
+        }
+
+        return ans;
+    }
+};
 
 // Not Optimal solution
 //  class Solution
