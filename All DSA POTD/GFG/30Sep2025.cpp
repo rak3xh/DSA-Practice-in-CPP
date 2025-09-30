de996b770d88944f89130589ea3fa2c428ef7cb8
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-09-30 22:16:36 +0530

    22:16 30 Sep

---
+++ All DSA POTD/GFG/30Sep2025.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string solve(int num, int n)
+    {
+        string ans;
+        while (num > 0)
+        {
+            ans.push_back('0' + (num % 2));
+            num /= 2;
+        }
+        reverse(ans.begin(), ans.end()); // fix order
+        // for leading zero pad the ans
+        while (ans.size() < n)
+        {
+            ans = '0' + ans;
+        }
+
+        return ans;
+    }
+    vector<string> binstr(int n)
+    {
+        // code here
+        int m = 1 << n;
+        vector<string> ans;
+        for (int i = 0; i < m; i++)
+        {
+            ans.push_back(solve(i, n));
+        }
+        return ans;
+    }
+};
