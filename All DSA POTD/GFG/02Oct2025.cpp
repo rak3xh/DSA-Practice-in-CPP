b945f0fb9f8a82f55d1f49a024dec7c7e8b59166
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-10-02 20:26:47 +0530

    20:06 02 Oct

---
+++ All DSA POTD/GFG/02Oct2025.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> ans;
+    void f(int n, int k, vector<int> v, int l)
+    {
+        if (v.size() == k)
+        {
+            if (n == 0)
+                ans.push_back(v);
+            return;
+        }
+
+        for (int i = l + 1; i <= 9; i++)
+        {
+            v.push_back(i);
+            f(n - i, k, v, i);
+            v.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(int n, int k)
+    {
+        // code here
+        vector<int> v;
+        f(n, k, v, 0);
+        return ans;
+    }
+};
