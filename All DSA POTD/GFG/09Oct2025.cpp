f9035cd4883cbfb57490d71e715a97566170ece9
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-10-09 20:59:45 +0530

    20:59 09 Oct

---
+++ All DSA POTD/GFG/09Oct2025.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    void post(Node *root, vector<int> &ans)
+    {
+        if (!root)
+            return;
+        post(root->left, ans);
+        post(root->right, ans);
+        ans.push_back(root->data);
+    }
+    vector<int> postOrder(Node *root)
+    {
+        // code here
+        vector<int> ans;
+        post(root, ans);
+        return ans;
+    }
+};
