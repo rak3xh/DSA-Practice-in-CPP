4e6118521746015b28484a3ede40868f27ba7512
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-10-10 20:28:04 +0530

    20:27 10 Oct

---
+++ All DSA POTD/GFG/10Oct2025.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> zigZagTraversal(Node *root)
+    {
+        // code here
+        vector<int> ans;
+        queue<Node *> q;
+        q.push(root);
+        bool f = 0;
+        while (!q.empty())
+        {
+            int size = q.size();
+            vector<int> v;
+            for (int i = 0; i < size; i++)
+            {
+                Node *front = q.front();
+                q.pop();
+                v.push_back(front->data);
+
+                if (front->left)
+                    q.push(front->left);
+                if (front->right)
+                    q.push(front->right);
+            }
+            if (f)
+                reverse(v.begin(), v.end());
+            f = !f;
+            ans.insert(ans.end(), v.begin(), v.end());
+        }
+        return ans;
+    }
+};
