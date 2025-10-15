61dd5803059e3cf04014cd18bf408035e11921ef
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-10-15 23:43:05 +0530

    23:42 15 Oct

---
+++ All DSA POTD/Leetcode/15Oct2025.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxIncreasingSubarrays(vector<int> &nums)
+    {
+        int ans = 0;
+        int increasing = 1;
+        int prevIncreasing = 0;
+
+        for (int i = 1; i < nums.size(); ++i)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                ++increasing;
+            }
+            else
+            {
+                prevIncreasing = increasing;
+                increasing = 1;
+            }
+            ans = max(ans, increasing / 2);
+            ans = max(ans, min(prevIncreasing, increasing));
+        }
+
+        return ans;
+    }
+};
