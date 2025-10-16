432050b5bc3c98ca4a73e834cab3f0a39da9567f
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-10-17 00:34:51 +0530

    00:34 17 Oct

---
+++ All DSA POTD/GFG/16Oct2025.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    Node *helper(Node *root, int l, int r)
+    {
+        if (!root)
+            return nullptr;
+
+        int val = root->data;
+        if (val < l)
+            return helper(root->right, l, r);
+        else if (val > r)
+            return helper(root->left, l, r);
+
+        Node *newNode = new Node(val);
+        newNode->left = helper(root->left, l, r);
+        newNode->right = helper(root->right, l, r);
+
+        return newNode;
+    }
+    Node *removekeys(Node *root, int l, int r)
+    {
+        // code here
+        return helper(root, l, r);
+    }
+};
