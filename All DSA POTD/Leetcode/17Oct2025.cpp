fe96c0a0acdb33ac244fdd84963ac61fbd2aa6de
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-10-18 00:51:06 +0530

    00:50 18 Oct

--- All DSA POTD/Leetcode/17Oct2025.cpp
+++ All DSA POTD/Leetcode/17Oct2025.cpp
@@ -54,51 +54,52 @@ private:
     }
 };
 // C++ 20 Solution
-#include <bits/stdc++.h>
-using namespace std;
-class Solution
-{
-public:
-    int maxPartitionsAfterOperations(string s, int k)
-    {
-        unordered_map<long, int> mem;
-        return maxPartitionsAfterOperations(s, 0, true, 0, k, mem) + 1;
-    }
 
-private:
-    // Returns the maximum number of partitions of s[i..n), where `canChange` is
-    // true if we can still change a letter, and `mask` is the bitmask of the
-    // letters we've seen.
-    int maxPartitionsAfterOperations(const string &s, int i, bool canChange,
-                                     int mask, int k,
-                                     unordered_map<long, int> &mem)
-    {
-        if (i == s.length())
-            return 0;
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Solution
+// {
+// public:
+//     int maxPartitionsAfterOperations(string s, int k)
+//     {
+//         unordered_map<long, int> mem;
+//         return maxPartitionsAfterOperations(s, 0, true, 0, k, mem) + 1;
+//     }
 
-        long key = static_cast<long>(i) << 27 | (canChange ? 1 : 0) << 26 | mask;
-        if (const auto it = mem.find(key); it != mem.end())
-            return it->second;
+// private:
+//     // Returns the maximum number of partitions of s[i..n), where `canChange` is
+//     // true if we can still change a letter, and `mask` is the bitmask of the
+//     // letters we've seen.
+//     int maxPartitionsAfterOperations(const string &s, int i, bool canChange,
+//                                      int mask, int k,
+//                                      unordered_map<long, int> &mem)
+//     {
+//         if (i == s.length())
+//             return 0;
 
-        // Initialize the result based on the current letter.
-        int res = getRes(s, i, canChange, mask, 1 << (s[i] - 'a'), k, mem);
+//         long key = static_cast<long>(i) << 27 | (canChange ? 1 : 0) << 26 | mask;
+//         if (const auto it = mem.find(key); it != mem.end())
+//             return it->second;
 
-        // If allowed, explore the option to change the current letter.
-        if (canChange)
-            for (int j = 0; j < 26; ++j)
-                res = max(res, getRes(s, i, false, mask, 1 << j, k, mem));
+//         // Initialize the result based on the current letter.
+//         int res = getRes(s, i, canChange, mask, 1 << (s[i] - 'a'), k, mem);
 
-        return mem[key] = res;
-    }
+//         // If allowed, explore the option to change the current letter.
+//         if (canChange)
+//             for (int j = 0; j < 26; ++j)
+//                 res = max(res, getRes(s, i, false, mask, 1 << j, k, mem));
 
-    int getRes(const string &s, int i, bool nextCanChange, unsigned mask,
-               int newBit, int k, unordered_map<long, int> &mem)
-    {
-        const unsigned newMask = mask | newBit;
-        if (popcount(newMask) > k) // fresh start
-            return 1 + maxPartitionsAfterOperations(s, i + 1, nextCanChange, newBit,
-                                                    k, mem);
-        return maxPartitionsAfterOperations(s, i + 1, nextCanChange, newMask, k,
-                                            mem);
-    }
-};
+//         return mem[key] = res;
+//     }
+
+//     int getRes(const string &s, int i, bool nextCanChange, unsigned mask,
+//                int newBit, int k, unordered_map<long, int> &mem)
+//     {
+//         const unsigned newMask = mask | newBit;
+//         if (popcount(newMask) > k) // fresh start
+//             return 1 + maxPartitionsAfterOperations(s, i + 1, nextCanChange, newBit,
+//                                                     k, mem);
+//         return maxPartitionsAfterOperations(s, i + 1, nextCanChange, newMask, k,
+//                                             mem);
+//     }
+// };
