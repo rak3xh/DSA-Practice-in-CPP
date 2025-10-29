e3395302fd9fe3cbacc5bc4b180e642baab5ba9e
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-10-29 23:16:41 +0530

    23:16 29 Oct

---
+++ All DSA POTD/Leetcode/29Oct2025.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int smallestNumber(int n)
+    {
+        return (1 << bitLength(n)) - 1;
+    }
+
+private:
+    int bitLength(int n)
+    {
+        return 32 - __builtin_clz(n);
+    }
+};
