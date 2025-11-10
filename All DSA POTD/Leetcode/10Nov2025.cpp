711a405698305ae36c6427829e38bdc31aa4b520
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-11-10 18:41:21 +0530

    18:41 10 Nov

---
+++ All DSA POTD/Leetcode/10Nov2025.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minOperations(vector<int> &nums)
+    {
+        int ans = 0;
+        stack<int> stack;
+        stack.push(0);
+
+        for (const int num : nums)
+        {
+            while (!stack.empty() && stack.top() > num)
+                stack.pop();
+            if (stack.empty() || stack.top() < num)
+            {
+                ++ans;
+                stack.push(num);
+            }
+        }
+
+        return ans;
+    }
+};
