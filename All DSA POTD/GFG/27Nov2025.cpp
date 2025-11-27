7958c7b6126e8bfd86b918ac2d2a54bd47ad0385
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-11-27 19:43:58 +0530

    19:43 27 Nov

---
+++ All DSA POTD/GFG/27Nov2025.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int solve(vector<int> &arr, int Xor, int i, vector<vector<int>> &dp)
+    {
+        if (i == arr.size())
+        {
+            return Xor;
+        }
+        if (dp[i][Xor] != -1)
+            return dp[i][Xor];
+        int take = solve(arr, Xor ^ arr[i], i + 1, dp);
+        int notTake = solve(arr, Xor, i + 1, dp);
+        return dp[i][Xor] = take + notTake;
+    }
+    int subsetXORSum(vector<int> &arr)
+    {
+        // code here
+        int n = arr.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2e3 + 1, -1));
+        return solve(arr, 0, 0, dp);
+    }
+};
