a55869694e17149d53ec2b7eece759f20e7b2a01
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-12-08 21:45:10 +0530

    21:45 08 Dec

--- All DSA POTD/Leetcode/08Dec2025.cpp
+++ All DSA POTD/Leetcode/08Dec2025.cpp
@@ -1,12 +1,37 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// class Solution
-// {
-// public:
-//     int countTriples(int n)
-//     {
-//         int ans = 0;
-//         unordered_set<int> squared;
+// C++ 14 Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int countTriples(int n)
+    {
+        int ans = 0;
+        unordered_set<int> squared;
+
+        for (int i = 1; i <= n; ++i)
+            squared.insert(i * i);
+
+        for (const int a : squared)
+            for (const int b : squared)
+                if (squared.count(a + b)) // replaced contains() with count()
+                    ++ans;
+
+        return ans;
+    }
+};
+
+// C++ 20 Solution
+//  #include <bits/stdc++.h>
+//  using namespace std;
+//  class Solution
+//  {
+//  public:
+//      int countTriples(int n)
+//      {
+//          int ans = 0;
+//          unordered_set<int> squared;
 
 //         for (int i = 1; i <= n; ++i)
 //             squared.insert(i * i);
