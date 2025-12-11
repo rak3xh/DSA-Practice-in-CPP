cf7fa0d8cd9d07740b910ee08fff1cec957307f1
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-12-11 23:47:42 +0530

    23:47 11 Dec

--- All DSA POTD/Leetcode/11Dec2025.cpp
+++ All DSA POTD/Leetcode/11Dec2025.cpp
@@ -1,63 +1,64 @@
-#include <bits/stdc++.h>
-using namespace std;
-class Solution
-{
-public:
-    int countCoveredBuildings(int n, vector<vector<int>> &buildings)
-    {
-        // Map to store all y-coordinates for each x-coordinate
-        unordered_map<int, vector<int>> buildingsAtX;
-        // Map to store all x-coordinates for each y-coordinate
-        unordered_map<int, vector<int>> buildingsAtY;
 
-        // Group buildings by their x and y coordinates
-        for (const auto &building : buildings)
-        {
-            int xCoord = building[0];
-            int yCoord = building[1];
-            buildingsAtX[xCoord].push_back(yCoord);
-            buildingsAtY[yCoord].push_back(xCoord);
-        }
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Solution
+// {
+// public:
+//     int countCoveredBuildings(int n, vector<vector<int>> &buildings)
+//     {
+//         // Map to store all y-coordinates for each x-coordinate
+//         unordered_map<int, vector<int>> buildingsAtX;
+//         // Map to store all x-coordinates for each y-coordinate
+//         unordered_map<int, vector<int>> buildingsAtY;
 
-        // Sort all y-coordinates for each x-coordinate group
-        for (auto &[x, yCoords] : buildingsAtX)
-        {
-            sort(yCoords.begin(), yCoords.end());
-        }
+//         // Group buildings by their x and y coordinates
+//         for (const auto &building : buildings)
+//         {
+//             int xCoord = building[0];
+//             int yCoord = building[1];
+//             buildingsAtX[xCoord].push_back(yCoord);
+//             buildingsAtY[yCoord].push_back(xCoord);
+//         }
 
-        // Sort all x-coordinates for each y-coordinate group
-        for (auto &[y, xCoords] : buildingsAtY)
-        {
-            sort(xCoords.begin(), xCoords.end());
-        }
+//         // Sort all y-coordinates for each x-coordinate group
+//         for (auto &[x, yCoords] : buildingsAtX)
+//         {
+//             sort(yCoords.begin(), yCoords.end());
+//         }
 
-        int coveredBuildingCount = 0;
+//         // Sort all x-coordinates for each y-coordinate group
+//         for (auto &[y, xCoords] : buildingsAtY)
+//         {
+//             sort(xCoords.begin(), xCoords.end());
+//         }
 
-        // Check each building to see if it's covered
-        for (const auto &building : buildings)
-        {
-            int xCoord = building[0];
-            int yCoord = building[1];
+//         int coveredBuildingCount = 0;
 
-            // Get all buildings with same x-coordinate (vertical line)
-            const vector<int> &yCoordinatesAtSameX = buildingsAtX[xCoord];
-            // Get all buildings with same y-coordinate (horizontal line)
-            const vector<int> &xCoordinatesAtSameY = buildingsAtY[yCoord];
+//         // Check each building to see if it's covered
+//         for (const auto &building : buildings)
+//         {
+//             int xCoord = building[0];
+//             int yCoord = building[1];
 
-            // Check if building is covered:
-            // - There exists a building to the left and right (on same y-coordinate)
-            // - There exists a building above and below (on same x-coordinate)
-            bool hasLeftBuilding = xCoordinatesAtSameY[0] < xCoord;
-            bool hasRightBuilding = xCoord < xCoordinatesAtSameY.back();
-            bool hasBottomBuilding = yCoordinatesAtSameX[0] < yCoord;
-            bool hasTopBuilding = yCoord < yCoordinatesAtSameX.back();
+//             // Get all buildings with same x-coordinate (vertical line)
+//             const vector<int> &yCoordinatesAtSameX = buildingsAtX[xCoord];
+//             // Get all buildings with same y-coordinate (horizontal line)
+//             const vector<int> &xCoordinatesAtSameY = buildingsAtY[yCoord];
 
-            if (hasLeftBuilding && hasRightBuilding && hasBottomBuilding && hasTopBuilding)
-            {
-                coveredBuildingCount++;
-            }
-        }
+//             // Check if building is covered:
+//             // - There exists a building to the left and right (on same y-coordinate)
+//             // - There exists a building above and below (on same x-coordinate)
+//             bool hasLeftBuilding = xCoordinatesAtSameY[0] < xCoord;
+//             bool hasRightBuilding = xCoord < xCoordinatesAtSameY.back();
+//             bool hasBottomBuilding = yCoordinatesAtSameX[0] < yCoord;
+//             bool hasTopBuilding = yCoord < yCoordinatesAtSameX.back();
 
-        return coveredBuildingCount;
-    }
-};
+//             if (hasLeftBuilding && hasRightBuilding && hasBottomBuilding && hasTopBuilding)
+//             {
+//                 coveredBuildingCount++;
+//             }
+//         }
+
+//         return coveredBuildingCount;
+//     }
+// };
