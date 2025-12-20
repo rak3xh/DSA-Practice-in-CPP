aef553eca55a1202796c1c9eb3ef36469cf746a4
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-12-20 20:38:23 +0530

    20:38 20 Dec

---
+++ All DSA POTD/Leetcode/20Dec2025.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minDeletionSize(vector<string> &strs)
+    {
+        int ans = 0;
+
+        for (int j = 0; j < strs[0].length(); ++j)
+            for (int i = 0; i + 1 < strs.size(); ++i)
+                if (strs[i][j] > strs[i + 1][j])
+                {
+                    ++ans;
+                    break;
+                }
+
+        return ans;
+    }
+};
