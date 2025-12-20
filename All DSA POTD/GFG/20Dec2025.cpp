9476b0c70158e0dfed6fcf515a378c29655ab2b0
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-12-20 20:39:12 +0530

    20:39 20 Dec

---
+++ All DSA POTD/GFG/20Dec2025.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int searchInsertK(vector<int> &arr, int k)
+    {
+        // code here
+        int n = arr.size();
+        int i = 0;
+        for (i = 0; i < n; ++i)
+        {
+            if (arr[i] == k)
+                return i;
+            if (arr[i] > k)
+                return i;
+        }
+        if (i == n)
+            return i;
+    }
+};
