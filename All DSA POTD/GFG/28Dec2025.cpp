3813bf7db07318119c8c5325e8549fa192a9caad
rak3xh <115797199+rak3xh@users.noreply.github.com>
2025-12-28 20:22:24 +0530

    20:22 28 Dec

--- All DSA POTD/GFG/28Dec2025.cpp
+++ All DSA POTD/GFG/28Dec2025.cpp
@@ -1,33 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 class Solution
 {
 public:
     struct cmp
     {
-        bool operator()(tuple<int, int, int> &t1, tuple<int, int, int> &t2)
+        bool operator()(const tuple<int, int, int> &t1,
+                        const tuple<int, int, int> &t2) const
         {
-            auto [a, b, c] = t1;
-            auto [x, y, z] = t2;
+            int a = get<0>(t1);
+            int b = get<1>(t1);
+
+            int x = get<0>(t2);
+            int y = get<1>(t2);
+
             return a + b > x + y;
         }
     };
+
     int minTime(vector<int> &ranks, int n)
     {
-        // code here
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, cmp> q;
+        priority_queue<
+            tuple<int, int, int>,
+            vector<tuple<int, int, int>>,
+            cmp>
+            q;
+
         for (int i : ranks)
-            q.push({0, i, i});
+            q.push(make_tuple(0, i, i));
+
         int ans = 0;
+
         while (n--)
         {
-            auto [a, b, c] = q.top();
+            tuple<int, int, int> t = q.top();
             q.pop();
+
+            int a = get<0>(t);
+            int b = get<1>(t);
+            int c = get<2>(t);
+
             a += b;
             b += c;
+
             ans = max(ans, a);
-            q.push({a, b, c});
+
+            q.push(make_tuple(a, b, c));
         }
+
         return ans;
     }
 };
