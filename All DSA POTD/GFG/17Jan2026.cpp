c0186fc0ea00d67810855fe5216d3b960ab06b9a
rak3xh <115797199+rak3xh@users.noreply.github.com>
2026-01-17 15:58:56 +0530

    15:58 17 Jan

---
+++ All DSA POTD/GFG/17Jan2026.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool checkRedundancy(string &s)
+    {
+        // code here
+        stack<char> st;
+        // int count=0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(')
+                st.push(s[i]);
+            else if (s[i] == ')')
+            {
+                int count = 0;
+                while (st.top() != '(')
+                {
+                    st.pop();
+                    count++;
+                }
+                st.pop();
+                if (count == 0 || count == 1)
+                    return true;
+            }
+            else
+            {
+                st.push(s[i]);
+            }
+        }
+        return false;
+    }
+};
