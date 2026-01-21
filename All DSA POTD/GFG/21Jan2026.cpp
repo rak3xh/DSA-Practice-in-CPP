693751471d26c2abf5b1d18eaa9884eef53f27cd
rak3xh <115797199+rak3xh@users.noreply.github.com>
2026-01-21 21:31:23 +0530

    21:31 21 Jan

---
+++ All DSA POTD/GFG/21Jan2026.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> calculateSpan(vector<int> &arr)
+    {
+        // code here
+        vector<int> res(arr.size());
+        stack<int> st;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            while (!st.empty() and arr[st.top()] <= arr[i])
+            {
+                st.pop();
+            }
+            if (st.empty())
+                res[i] = i + 1;
+            else
+                res[i] = (i - st.top());
+            st.push(i);
+        }
+        return res;
+    }
+};
