080c1bd48a7667e4ee4233cddab19ac96f37b44c
rak3xh <115797199+rak3xh@users.noreply.github.com>
2026-01-25 20:36:58 +0530

    20:36 25 Jan

---
+++ All DSA POTD/Leetcode/25Jan2026.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int minimumDifference(vector<int> &nums, int k)
+    {
+        sort(nums.begin(), nums.end());
+
+        int ans = nums[k - 1] - nums[0];
+
+        for (int i = k; i < nums.size(); ++i)
+            ans = min(ans, nums[i] - nums[i - k + 1]);
+
+        return ans;
+    }
+};
