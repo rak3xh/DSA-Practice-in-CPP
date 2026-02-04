0eaa775d12aedc57540e09fdf5ec7125cb62b6e5
rak3xh <115797199+rak3xh@users.noreply.github.com>
2026-02-04 21:08:55 +0530

    21:08 04 Feb

---
+++ All DSA POTD/GFG/04Feb2026.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int getLastMoment(int n, vector<int> &left, vector<int> &right)
+    {
+        // code here
+        int maxtime = 0;
+
+        for (int i = 0; i < left.size(); i++)
+        {
+            maxtime = max(maxtime, left[i]);
+        }
+
+        for (int i = 0; i < right.size(); i++)
+        {
+            maxtime = max(maxtime, n - right[i]);
+        }
+
+        return maxtime;
+    }
+};
