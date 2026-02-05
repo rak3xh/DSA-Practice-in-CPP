d5803a88fc08624ec57b40d41d36b9378c245fb3
rak3xh <115797199+rak3xh@users.noreply.github.com>
2026-02-05 21:25:57 +0530

    21:25 05 Feb

---
+++ All DSA POTD/Leetcode/05Feb2026.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> constructTransformedArray(vector<int> &nums)
+    {
+        const int n = nums.size();
+        vector<int> ans;
+
+        for (int i = 0; i < n; ++i)
+            ans.push_back(nums[(i + nums[i] % n + n) % n]);
+
+        return ans;
+    }
+};
