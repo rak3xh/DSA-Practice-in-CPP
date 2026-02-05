c0d8651ebf884370c1f117dd72788aea267738b4
rak3xh <115797199+rak3xh@users.noreply.github.com>
2026-02-05 21:26:53 +0530

    21:26 05 Feb

---
+++ All DSA POTD/GFG/05Feb2026.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxOnes(vector<int> &arr, int k)
+    {
+        // code here
+        int n = arr.size();
+        int last_zero = 0;
+        int current = 0;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == 0)
+                current++;
+            if (current > k)
+            {
+                while (arr[last_zero] != 0)
+                    last_zero++;
+                last_zero++;
+                current--;
+            }
+            ans = max(ans, i - last_zero + 1);
+        }
+        ans = max(ans, n - last_zero);
+        return ans;
+    }
+};
