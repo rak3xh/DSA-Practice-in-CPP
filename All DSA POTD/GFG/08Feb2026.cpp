88e4a739c693899327d3e31d1b035d20dd3c362f
rak3xh <115797199+rak3xh@users.noreply.github.com>
2026-02-08 16:33:49 +0530

    16:33 08 Feb

---
+++ All DSA POTD/GFG/08Feb2026.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int maxProduct(vector<int> &arr)
+    {
+        // code here
+        int n = arr.size(), ans = INT_MIN;
+        int pref = 1, suff = 1;
+        for (int i = 0; i < n; i++)
+        {
+            pref *= arr[i];
+            suff *= arr[n - i - 1];
+            ans = max({ans, pref, suff});
+            if (pref == 0)
+                pref = 1;
+            if (suff == 0)
+                suff = 1;
+        }
+        return ans;
+    }
+};
