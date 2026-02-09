3ce9f0da9529a350094af5a7f77149d601defdd1
rak3xh <115797199+rak3xh@users.noreply.github.com>
2026-02-09 20:33:45 +0530

    20:33 09 Feb

---
+++ All DSA POTD/GFG/09Feb2026.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int findKRotation(vector<int> &arr)
+    {
+        // Code Here
+        int start = 0;
+        int end = arr.size() - 1;
+        int ans = 0;
+        int n = arr.size();
+
+        while (start <= end)
+        {
+
+            int mid = start + ((end - start) / 2);
+
+            if (arr[mid] == 1)
+            {
+                ans = mid;
+                break;
+            }
+            else if (arr[mid] > arr[n - 1])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                ans = mid;
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
