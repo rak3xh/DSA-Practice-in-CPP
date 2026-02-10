5bd9e015b95215cf50eef81e1724c8990e5ca91c
rak3xh <115797199+rak3xh@users.noreply.github.com>
2026-02-10 21:45:44 +0530

    21:45 10 Feb

---
+++ All DSA POTD/GFG/10Feb2026.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int n;
+
+    int check(int t, vector<int> &arr)
+    {
+        {
+            int x = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if ((arr[i] % t) == 0)
+                {
+                    x += (arr[i] / t);
+                }
+                else
+                    x += ((arr[i] + t) / t);
+            }
+
+            return x;
+        }
+    }
+    int kokoEat(vector<int> &arr, int k)
+    {
+        // Code here
+        n = arr.size();
+
+        int lo = 1, hi = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            hi = max(hi, arr[i]);
+        }
+
+        int ans = hi;
+
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            if (check(mid, arr) <= k)
+            {
+                ans = mid;
+                hi = mid - 1;
+            }
+            else
+                lo = mid + 1;
+        }
+
+        return ans;
+    }
+};
