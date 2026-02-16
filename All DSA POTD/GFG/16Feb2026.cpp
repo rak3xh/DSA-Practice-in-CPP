3759bafffe4bdeb8c803992433cd665aa9872f28
rak3xh <115797199+rak3xh@users.noreply.github.com>
2026-02-16 23:41:37 +0530

    23:41 16 Feb

---
+++ All DSA POTD/GFG/16Feb2026.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool canAttend(vector<vector<int>> &arr)
+    {
+        // Code Here
+        sort(arr.begin(), arr.end());
+        for (int i = 1; i < arr.size(); i++)
+        {
+            if (arr[i - 1][1] > arr[i][0])
+                return false;
+        }
+        return true;
+    }
+};
