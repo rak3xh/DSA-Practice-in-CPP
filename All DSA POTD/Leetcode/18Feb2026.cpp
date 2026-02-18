b92996c23f811a693c083722952fbc8480e83d47
rak3xh <115797199+rak3xh@users.noreply.github.com>
2026-02-19 00:07:22 +0530

    00:07 19 Feb

---
+++ All DSA POTD/Leetcode/18Feb2026.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool hasAlternatingBits(int n)
+    {
+        //            n = 0b010101
+        //       n >> 2 = 0b000101
+        // n ^ (n >> 2) = 0b010000 = a
+        //        a - 1 = 0b001111
+        //  a & (a - 1) = 0
+        const int a = n ^ (n >> 2);
+        return (a & (a - 1)) == 0;
+    }
+};
